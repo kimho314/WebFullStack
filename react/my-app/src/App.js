c4aadfb8dc05f785ff3551d03a1efacc9dc70549
Randy Kim <kimho314@gamil.com>
2021-03-01 11:16:59 +0900

    add react directory

---
+++ react/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Hello from './Hello';
+// import Wrapper from './Wrapper';
+import Counter from './Counter';
+
+function App() {
+  return (
+    // <Wrapper>
+    //   <Hello color='pink' />
+    //   <Hello name='react' color='red' isSpecial={true} />
+    // </Wrapper>
+    <Counter />
+  );
+}
+
+export default App;
