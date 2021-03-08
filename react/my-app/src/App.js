de8bf8d1211403f2f5f5459bc0fde2fc7a56e8c2
Hoseop.Kim <kimho314@gamil.com>
2021-03-08 20:50:03 +0900

    modify App.js, UserList.js

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo, useCallback } from 'react';
 // import Hello from './Hello';
 // import Wrapper from './Wrapper';
 import Counter from './Counter';
@@ -6,29 +6,25 @@ import CreateUser from './CreateUser';
 import InputSample from './inputSample';
 import UserList from './UserList';
 
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.fileter(user => user.active).length;
+}
+
 function App() {
   const [inputs, setInputs] = useState({
     username: '',
     email: '',
   });
 
-  const onChange = e => {
+  const onChange = useCallback(e => {
     const { name, value } = e.target;
     setInputs({
       ...inputs,
       [name]: value
     });
-  };
+  }, [inputs]);
 
-  const onRemove = id => {
-    setUsers(users.fileter(user => user.id !== id));
-  };
-
-  const onToggle = id => {
-    setUsers(users.map(
-      user => user.id === id ? { ...user, active: !user.active } : user
-    ));
-  };
 
   const { username, email } = inputs;
 
@@ -55,11 +51,24 @@ function App() {
 
   const nextId = useRef(4); // rerendering되도 값이 바뀌지 않음
 
-  const onCreate = () => {
+  const onRemove = useCallback(id => {
+    setUsers(users.fileter(user => user.id !== id));
+  }, [users]);
+
+  const onToggle = useCallback(id => {
+    setUsers(users.map(
+      user => user.id === id ? { ...user, active: !user.active } : user
+    ));
+  }, [users]);
+
+
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
-      ...inputs,
-    }
+      // ...inputs,
+      username,
+      email,
+    };
 
     /*
     새로운 배열을 추가할때는 새로운 배열을 만들고 만들어진 새로운 배열에 추가하는 방식을 사용
@@ -79,7 +88,9 @@ function App() {
       email: '',
     });
     nextId.current += 1;
-  }
+  }, [username, email, users])
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     // <Wrapper>
@@ -91,6 +102,7 @@ function App() {
     <>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>솰성 사용자 수: {count}</div>
     </>
   );
 }
--- react/my-app/src/UserList.js
+++ react/my-app/src/UserList.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
     const { username, email, id, active } = user;
+
+    // useEffect(() => {
+    //     console.log('컴포넌트가 화면에 나타남');
+    //     // props -> state
+    //     // request REST API
+    //     // D3, Video.js
+    //     // setInterval, setTimeout
+    //     // access DOM
+    //     return () => {
+    //         // clearInterval, clearTimeout
+    //         // remove library instance
+    //         console.log('컴포넌트가 화면에서 사라짐');
+    //     }
+    // }, []);
+
+    useEffect(() => {
+        console.log('user 값이 설정됨');
+        console.log(user);
+        return () => {
+            console.log('user 값이 바뀌기 전');
+            console.log(user);
+        }
+    }, [user]); // user값이 업데이트될때 호출
+
     return (
         <div>
             <b style={{
