a28c3851ed80868bdf941ac661dead5185a53cb2
Hoseop.Kim <kimho314@gamil.com>
2021-03-28 15:32:53 +0900

    add sub router example

--- react/router-tutorial/src/App.js
+++ react/router-tutorial/src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profile from './Profile';
+import Profiles from './Profiles';
 
 function App() {
   return (
@@ -14,11 +15,14 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
