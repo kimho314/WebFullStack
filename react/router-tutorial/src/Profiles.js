6979b6f476b92fb0bca1073edd0a672ee7ce3b04
Hoseop.Kim <kimho314@gamil.com>
2021-03-29 21:03:10 +0900

    add router sample

--- react/router-tutorial/src/Profiles.js
+++ react/router-tutorial/src/Profiles.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Profile from './Profile';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Link, Route } from 'react-router-dom';
+import WithRouterSample from './WithRoutersample';
 
 function Profiles() {
     return (
         <div>
             <h3>사용자 목록</h3>
             <ul>
-                <li><Link to="/profiles/velopert">velopert</Link></li>
-                <li><Link to="/rpofiles/homer">homer</Link></li>
+                <li><NavLink to="/profiles/velopert"
+                    activeStyle={{ background: 'black', color: 'white' }}>velopert</NavLink></li>
+                <li><NavLink to="/rpofiles/homer"
+                    activeStyle={{ background: 'black', color: 'white' }}>homer</NavLink></li>
             </ul>
 
             <Route path="/profiles" exact render={() => <div>사용자를 선택해주세요</div>}></Route>
             <Route path="/profiles/:username" component={Profile} />
+            <WithRouterSample />
         </div>
     )
 }
