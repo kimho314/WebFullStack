a42c2a6961c9915e05e38290aaa762b4ebbc866c
Hoseop.Kim <kimho314@gamil.com>
2021-07-31 16:16:45 +0900

    add filter project

---
+++ filter/src/main/java/com/example/filter/dto/User.java
@@ -0,0 +1,15 @@
+package com.example.filter.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
