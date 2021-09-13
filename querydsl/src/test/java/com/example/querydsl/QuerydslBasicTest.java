053e9a1351b4ccae59480ba56605e61adf88768a
Hoseop.Kim <kimho314@gmail.com>
2021-09-13 22:59:15 +0900

    - delete gradle_multi_module directory - modify QuerydslBasicTest

--- querydsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -336,4 +336,27 @@ public class QuerydslBasicTest {
                 .fetch();
         result.forEach(System.out::println);
     }
+
+    @Test
+    public void simpleProjection(){
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    public void tupleProjection(){
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+        for(Tuple tuple : result){
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
 }
