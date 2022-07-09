fc306a2ecaf8498c5a51e0dc716889912cac2bfd
Hoseop.Kim <kimho314@gmail.com>
2022-07-09 09:00:00 +0900

    add jobParameter example with StepScope and JobSceop annotations

--- spring_batch/spring-batch-example/src/main/java/fastcampus/spring/batch/part3/ChunkProcessingConfiguration.java
+++ spring_batch/spring-batch-example/src/main/java/fastcampus/spring/batch/part3/ChunkProcessingConfiguration.java
@@ -3,20 +3,29 @@ package fastcampus.spring.batch.part3;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Configuration
 @Slf4j
@@ -37,14 +46,16 @@ public class ChunkProcessingConfiguration {
         return jobBuilderFactory.get("chunkProcessingJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.taskBaseStep())
-                .next(this.chunkBaseStep())
+                .next(this.chunkBaseStep(null))
                 .build();
     }
 
     @Bean
-    public Step chunkBaseStep(){
+    @JobScope
+    public Step chunkBaseStep(@Value("#{jobParameters[chunkSize]}") String chunkSize){
         return stepBuilderFactory.get("chunkBaseStep")
-                .<String, String>chunk(10) // 1번째 String : processor 의 input type, 2번째 String : processor 의 return type
+//                .<String, String>chunk(10) // 1번째 String : processor 의 input type, 2번째 String : processor 의 return type
+                .<String, String>chunk(StringUtils.isNotEmpty(chunkSize) ? Integer.parseInt(chunkSize) : 10) // 1번째 String : processor 의 input type, 2번째 String : processor 의 return type
                 .reader(itemReader())
                 .processor(itemProcessor())
                 .writer(itemWriter())
@@ -82,14 +93,18 @@ public class ChunkProcessingConfiguration {
 
     /**
      * chunk-based batch 와 비슷하게 동작함
-     * tasklet 기반에서 chunk-based 와 같이 동작하게 하려면 구현이 복잡함 -> chunk-based batch가 깔끔!!!
+     * tasklet 기반에서 chunk-based 와 같이 동작하게 하려면 구현이 복잡함 -> chunk-based batch 가 깔끔!!!
      */
-    private Tasklet tasklet() {
+    @Bean
+    @StepScope
+    public Tasklet tasklet() {
         List<String> items = getItems();
         return (contribution, chunkContext) -> {
             StepExecution stepExecution = contribution.getStepExecution();
+            JobParameters jobParameters = stepExecution.getJobParameters();
 
-            int chunkSize = 10;
+            String value = jobParameters.getString("chunkSize", "10");
+            int chunkSize = StringUtils.isNotBlank(value) ? Integer.parseInt(value) : 10;
             int fromIndex = stepExecution.getReadCount();
             int toIndex = fromIndex + chunkSize;
 
@@ -99,6 +114,7 @@ public class ChunkProcessingConfiguration {
 
             List<String> subList = items.subList(fromIndex, toIndex);
             log.info("task item size : {}", subList.size());
+            stepExecution.setReadCount(toIndex);
 
             return RepeatStatus.CONTINUABLE;
         };
