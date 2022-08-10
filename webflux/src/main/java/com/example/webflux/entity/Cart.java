5b13d56680cdedbea296c3b309355d69fc28232f
hoseop.kim <kimho314@gmail.com>
2022-08-10 23:16:26 +0900

    - add @Document annotion on Item, Cart entities

--- webflux/src/main/java/com/example/webflux/entity/Cart.java
+++ webflux/src/main/java/com/example/webflux/entity/Cart.java
@@ -2,10 +2,12 @@ package com.example.webflux.entity;
 
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Collections;
 import java.util.List;
 
+@Document
 @Builder
 public record Cart(@Id String id,
                    List<CartItem> cartItems) {
--- webflux/src/main/java/com/example/webflux/entity/Item.java
+++ webflux/src/main/java/com/example/webflux/entity/Item.java
@@ -2,7 +2,9 @@ package com.example.webflux.entity;
 
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 @Builder
 public record Item(@Id String id,
                    String name,
