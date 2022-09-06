bf74ef15ff222d50bb34353fe7feb828345e4923
Hoseop.Kim <kimho314@gmail.com>
2022-09-06 22:31:41 +0900

    - add HomeControllerSliceTest

--- webflux/src/main/java/com/example/webflux/service/InventoryService.java
+++ webflux/src/main/java/com/example/webflux/service/InventoryService.java
@@ -1,21 +1,26 @@
 package com.example.webflux.service;
 
+import com.example.webflux.entity.Cart;
 import com.example.webflux.entity.Item;
+import com.example.webflux.repository.CartRepository;
 import com.example.webflux.repository.ItemByExampleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
 public class InventoryService {
 
     private final ItemByExampleRepository itemByExampleRepository;
+    private final CartRepository cartRepository;
 
-    public InventoryService(ItemByExampleRepository itemByExampleRepository) {
+    public InventoryService(ItemByExampleRepository itemByExampleRepository, CartRepository cartRepository) {
         this.itemByExampleRepository = itemByExampleRepository;
+        this.cartRepository = cartRepository;
     }
 
     public Flux<Item> searchByExample(String name, String description, boolean useAnd) {
@@ -30,5 +35,15 @@ public class InventoryService {
         var probe = Example.of(item, matcher);
 
         return itemByExampleRepository.findAll(probe);
+    }
+
+    public Flux<Item> getInventory() {
+        var inventories = itemByExampleRepository.findAll();
+        return inventories;
+    }
+
+    public Mono<Cart> getCart(String id) {
+        var cart = cartRepository.findById(id);
+        return cart;
     }
 }
---
+++ webflux/src/test/java/com/example/webflux/HomeControllerSliceTest.java
@@ -0,0 +1,50 @@
+package com.example.webflux;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.example.webflux.controller.HomeController;
+import com.example.webflux.entity.Cart;
+import com.example.webflux.entity.Item;
+import com.example.webflux.repository.CartRepository;
+import com.example.webflux.repository.ItemRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@WebFluxTest(HomeController.class)
+public class HomeControllerSliceTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    //    @MockBean
+//    InventoryService inventoryService;
+    @MockBean
+    CartRepository cartRepository;
+    @MockBean
+    ItemRepository itemRepository;
+
+    @Test
+    void homePageTest() {
+        Mockito.when(itemRepository.findAll())
+                .thenReturn(Flux.just(
+                        Item.builder().id("id1").name("name1").description("desc1").price(1.99).build(),
+                        Item.builder().id("id2").name("name2").description("desc2").price(3.99).build()
+                ));
+        Mockito.when(cartRepository.findById("My Cart"))
+                .thenReturn(Mono.just(Cart.builder().id("My Cart").build()));
+
+        client.get().uri("/").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .consumeWith(exchangeResult -> {
+                    assertThat(exchangeResult.getResponseBody()).contains("action=\"/add/id1\"");
+                    assertThat(exchangeResult.getResponseBody()).contains("action=\"/add/id2\"");
+                });
+    }
+}
