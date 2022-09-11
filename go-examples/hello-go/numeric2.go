9fa951f394a2187e76199baa108ebffc1cd2c636
Hoseop.Kim <kimho314@gmail.com>
2022-09-11 22:25:23 +0900

    add numeric example

--- go-examples/hello-go/numeric2.go
+++ go-examples/hello-go/numeric2.go
@@ -10,4 +10,12 @@ func main() {
 	fmt.Println("char1 : ", char1)
 	fmt.Println("char2 : ", char2)
 	fmt.Println("char3 : ", char3)
+
+	var char4 rune = 50556
+	var char5 rune = 0142574
+	var char6 rune = 0xC57C
+
+	fmt.Printf("%c %c %c\n", char4, char5, char6)
+	fmt.Printf("%d %d %d\n", char4, char5, char6)
+	fmt.Printf("%d %o %x\n", char4, char5, char6)
 }
