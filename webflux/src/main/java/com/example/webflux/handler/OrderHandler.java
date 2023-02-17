c8b52a9d000f40f4aed082c86db37fa3fc2b6376
Hoseop.Kim <kimho314@gmail.com>
2023-02-17 23:16:39 +0900

    - add OrderHandler

---
+++ webflux/src/main/java/com/example/webflux/handler/OrderHandler.java
@@ -0,0 +1,22 @@
+package com.example.webflux.handler;
+
+import com.example.webflux.dto.Order;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+public class OrderHandler {
+
+    public Mono<ServerResponse> getOrders(ServerRequest serverRequest) {
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(Flux.just(new Order(1L, 100.0),
+                                new Order(2L, 200.0),
+                                new Order(3L, 300.0)),
+                        Order.class);
+    }
+}
