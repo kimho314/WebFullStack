c554361b56067db7e7b4d7c5eebdfe49338cdb7e
Hoseop.Kim <kimho314@gmail.com>
2023-03-26 22:45:37 +0900

    add first-ex.py

---
+++ python-example/first-ex.py
@@ -0,0 +1,30 @@
+import random
+import math
+
+num = random.randint(16,272)
+
+print(num)
+print(num + 1)
+
+print(type(5))
+
+print(int(3.9))
+
+loc2 = int(3.9)
+print(loc2)
+destination = loc2 - 1
+print(destination)
+
+print(3+2)
+n = (3+2)
+print(n)
+n2 = n -5 
+print(n2)
+print(3==2)
+
+print(destination%loc2)
+x = 3.14159
+y = 2.2
+area = x* (y**2)
+# print(pi* (radius**2))
+print(area)
