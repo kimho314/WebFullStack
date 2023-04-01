f812c4726324ae824f34392e1e2efbc981952ed7
Hoseop.Kim <kimho314@gmail.com>
2023-04-01 22:13:10 +0900

    modify BookingHotel

--- cs-algorithms/BookingHotel.java
+++ cs-algorithms/BookingHotel.java
@@ -1,43 +1,61 @@
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/155651
  */
 public class BookingHotel {
+    private static final String BREAK_TIME = "10";
 
     public static void main(String[] args) {
         String[][] input1 = {{"15:00", "17:00"}, {"16:40", "18:20"}, {"14:20", "15:20"}, {"14:10", "19:20"},
-            {"18:20", "21:20"}};
+                {"18:20", "21:20"}};
         int solution = solution(input1);
+        System.out.println(solution);
+        System.out.println("========");
+        String[][] input2 = {{"09:10", "10:10"}, {"10:20", "12:20"}};
+        int solution2 = solution(input2);
+        System.out.println(solution2);
+        System.out.println("========");
+        String[][] input3 = {{"10:20", "12:30"}, {"10:20", "12:30"}, {"10:20", "12:30"}};
+        int solution3 = solution(input3);
+        System.out.println(solution3);
+        System.out.println("========");
+        String[][] input4 = {{"00:00", "12:30"}};
+        int solution4 = solution(input4);
+        System.out.println(solution4);
     }
 
     public static int solution(String[][] book_time) {
-        int answer = 0;
-        final String breakTime = "00:10";
-        List<String[]> sortedBookTime = getSortedList(book_time);
-        for (int i = 0; i < sortedBookTime.size() - 1; i++) {
-            String time2 = sortedBookTime.get(i)[1];
-            String[] split = time2.split(":");
-            int addedMin = Integer.parseInt(split[1]) + Integer.parseInt(breakTime.split(":")[1]);
-            String joinTime = String.join(":", split[0], String.valueOf(addedMin));
-            System.out.println("joinTime = " + joinTime);
-
-            for (int j = i + 1; j < sortedBookTime.size(); j++) {
-                
-            }
+        // [입실시간, 정리완료시간] 을 넣을 int 2차배열 생성 및 값 주입
+        int[][] bookTime = new int[book_time.length][];
+        for (int i = 0; i < book_time.length; i++) {
+            bookTime[i] = new int[]{parseTime(book_time[i][0]), parseTime(book_time[i][1]) + 10};
         }
-        return answer;
+
+        // 입실시간 오름차순 정렬
+        Arrays.sort(bookTime, (a, b) -> a[0] - b[0]);
+
+        // 현재 이용중인 방의 퇴실시간정보 사용을 위해 퇴실시간 min-heap 선언
+        PriorityQueue<int[]> inUse = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        int ans = 0;
+
+        for (int[] ints : bookTime) {
+
+            // 다음입실시간 까지 정리가 완료되는방 poll처리
+            while (!inUse.isEmpty() && inUse.peek()[1] <= ints[0]) {
+                inUse.poll();
+            }
+            inUse.add(ints);
+            ans = Math.max(ans, inUse.size());
+        }
+        return ans;
     }
 
-    private static List<String[]> getSortedList(String[][] book_time) {
-        List<String[]> strings = Arrays.stream(book_time)
-            .peek(s -> System.out.println(s[0]))
-            .sorted(Comparator.comparing(s -> s[0]))
-            .toList();
-        System.out.println("==========");
-        strings.forEach(s -> System.out.println(s[0]));
-        return strings;
+    // time String을 int값으로 변환
+    public static int parseTime(String time) {
+        String[] hhmm = time.split(":");
+        int hour = Integer.parseInt(hhmm[0]), min = Integer.parseInt(hhmm[1]);
+        return hour * 60 + min;
     }
 }
