68e150055c434749597c5574bd10a32c6a6f828c
Hoseop.Kim <kimho314@gmail.com>
2023-04-09 13:55:54 +0900

    modify Painting

--- cs-algorithms/Painting.java
+++ cs-algorithms/Painting.java
@@ -1,25 +1,58 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Painting {
     public static void main(String[] args) {
         int n = 8, m = 4;
         int[] section = {2, 3, 6};
-        System.out.println(new Painting().solution(n, m, section));
+        System.out.println(new Painting().solution2(n, m, section));
         n = 5;
         m = 4;
-        section = new int[]{2, 3, 6};
-        System.out.println(new Painting().solution(n, m, section));
+        section = new int[]{1, 3};
+        System.out.println(new Painting().solution2(n, m, section));
         n = 4;
         m = 1;
         section = new int[]{1, 2, 3, 4};
-        System.out.println(new Painting().solution(n, m, section));
+        System.out.println(new Painting().solution2(n, m, section));
     }
 
     public int solution(int n, int m, int[] section) {
         int answer = 0;
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < section.length; i++) {
-            answer++;
-            int nextNum = section[i] + (m - 1);
-            if (nextNum >= n || nextNum >= section[section.length - 1]) {
+            list.add(section[i]);
+        }
+        for (int i = 1; i <= n; i++) {
+            if (i > section[section.length - 1]) {
                 break;
+            }
+            if (!list.contains(i)) {
+                continue;
+            }
+
+            if (!set.isEmpty() && set.contains(i)) {
+                continue;
+            }
+
+            int limit = i + (m - 1);
+            set.clear();
+            for (int j = i; j <= limit; j++) {
+                set.add(j);
+            }
+
+            answer++;
+        }
+        return answer;
+    }
+
+    public int solution2(int n, int m, int[] section) {
+        int answer = 1;
+        int roller = section[0];
+        for (int i = 1; i < section.length; i++) {
+            if (roller + m - 1 < section[i]) {
+                answer++;
+                roller = section[i];
             }
         }
         return answer;
