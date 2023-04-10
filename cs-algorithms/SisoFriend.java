effab4dcc00611cfbb8a99b58c9dfd2d9a2ab233
Hoseop.Kim <kimho314@gmail.com>
2023-04-10 22:33:34 +0900

    modify SisoFriend

--- cs-algorithms/SisoFriend.java
+++ cs-algorithms/SisoFriend.java
@@ -1,20 +1,28 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SisoFriend {
     public static void main(String[] args) {
-        int[] distances = {2, 3, 4};
-        Set<Double> ratio = new HashSet<>();
-        for (int i = 0; i < distances.length; i++) {
-            for (int j = 0; j < distances.length; j++) {
-                ratio.add((double) distances[i] / (double) distances[j]);
-            }
-        }
-        ratio.forEach(System.out::println);
+        int[] weights = {100, 180, 360, 100, 270};
+        System.out.println(new SisoFriend().solution(weights));
     }
 
     public long solution(int[] weights) {
         long answer = 0;
+        double[] ratios = {1.0, 2.0, 3.0 / 2.0, 4.0 / 3.0, 1.0 / 2.0, 2.0 / 3.0, 3.0 / 4.0};
+        // key : weight, value : count
+        Map<Double, Integer> map = new HashMap<>();
+
+        for (double w : weights) {
+            answer += map.getOrDefault(w * ratios[0], 0)
+                    + map.getOrDefault(w * ratios[1], 0)
+                    + map.getOrDefault(w * ratios[2], 0)
+                    + map.getOrDefault(w * ratios[3], 0)
+                    + map.getOrDefault(w * ratios[4], 0)
+                    + map.getOrDefault(w * ratios[5], 0)
+                    + map.getOrDefault(w * ratios[6], 0);
+            map.put(w, map.getOrDefault(w, 0) + 1);
+        }
         return answer;
     }
 }
