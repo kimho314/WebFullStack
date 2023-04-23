f120f27de290f966e061f6ed26cda2dea46156b7
Hoseop.Kim <kimho314@gmail.com>
2023-04-23 22:23:24 +0900

    add CantorBitArray

---
+++ cs-algorithms/CantorBitArray.java
@@ -0,0 +1,13 @@
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/148652
+ */
+public class CantorBitArray {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int n, long l, long r) {
+        int answer = 0;
+        return answer;
+    }
+}
