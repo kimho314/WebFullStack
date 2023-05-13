f9a89e08914e22b865d6ea78bb86a351924fe4cd
Hoseop.Kim <kimho314@gmail.com>
2023-05-14 00:17:49 +0900

    add Permutation

--- cs-algorithms/CandidateKey.java
+++ cs-algorithms/CandidateKey.java
@@ -8,6 +8,18 @@ public class CandidateKey {
 
     public int solution(String[][] relation) {
         int answer = 0;
+
+        // get all permutation of column combination
+        // column : 1, 2, 3
+        // 1, 2, 3, 12, 13, 23, 123
+        // column : 1, 2, 3, 4, 5
+        // 1, 2, 3, 4, 5, 12, 13, 14, 15, 23, 24, 25, 34, 35, 45, 123, 124, 125, 234, 235, 345, 1234, 1235, 2345, 12345
+
+
+        // for loop permutation of column combination
+        // check if column combination is candidate key
+        // condition for candidate key : uniqueness
+
         return answer;
     }
 }
