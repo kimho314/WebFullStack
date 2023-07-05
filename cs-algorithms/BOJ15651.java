e58ddda62dc2ade52a2f63df70c5f8838ceb031a
Hoseop.Kim <kimho314@gmail.com>
2023-07-05 13:06:45 +0900

    modify BOJ15651

--- cs-algorithms/BOJ15651.java
+++ cs-algorithms/BOJ15651.java
@@ -1,36 +1,29 @@
-import java.util.Scanner;
-
 public class BOJ15651 {
     static StringBuilder sb = new StringBuilder();
-    static Scanner sc = new Scanner(System.in);
-
-    static int N, M;
+    static FastReader sc = new FastReader();
+    static int N;
+    static int M;
     static int[] selected;
 
     public static void main(String[] args) {
         N = sc.nextInt();
         M = sc.nextInt();
         selected = new int[M + 1];
-
-        rec_func(1);
+        rec_fun(1);
         System.out.println(sb);
     }
 
-    static void rec_func(int k) {
-        if (k == M + 1) { // 다 골랐다!
-            // selected[1...M] 배열이 새롭게 탐색된 결과
+    static void rec_fun(int k) {
+        if (k == M + 1) {
             for (int i = 1; i <= M; i++) {
-                sb.append(selected[i]).append(' ');
+                sb.append(selected[i]).append(" ");
             }
-            sb.append('\n');
+            sb.append("\n");
         }
         else {
             for (int cand = 1; cand <= N; cand++) {
-                // k 번째에 cand 가 올 수 있으면
                 selected[k] = cand;
-
-                // k+1 번부터 M 번까지 잘 채워주는 함수를 호출해준다.
-                rec_func(k + 1);
+                rec_fun(k + 1);
                 selected[k] = 0;
             }
         }
