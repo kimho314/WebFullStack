d3fc0e3bc9fcc642ec2137bc1f9972273463219d
Hoseop.Kim <kimho314@gmail.com>
2023-07-11 22:27:06 +0900

    modify BOJ14888

--- cs-algorithms/BOJ14888.java
+++ cs-algorithms/BOJ14888.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class BOJ14888 {
     static int N;
     static int[] NUMS;
@@ -7,7 +5,7 @@ public class BOJ14888 {
     static int[] ORDERS;
     static int MAX = Integer.MIN_VALUE;
     static int MIN = Integer.MAX_VALUE;
-    static Scanner sc = new Scanner(System.in);
+    static FastReader sc = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
@@ -22,76 +20,39 @@ public class BOJ14888 {
             OPERATORS[i] = sc.nextInt();
         }
 
-//        rec_func(1);
-        rec_func2(1, NUMS[1]);
+        recFunc(1);
         sb.append(MAX).append("\n").append(MIN);
         System.out.println(sb);
     }
 
-    static int calc() {
-        int value = NUMS[0];
-        for (int i = 0; i < N; i++) {
-            if (ORDERS[i] == 0) {
-                value += NUMS[i + 1];
-            }
-            else if (ORDERS[i] == 1) {
-                value -= NUMS[i + 1];
-            }
-            else if (ORDERS[i] == 2) {
-                value *= NUMS[i + 1];
-            }
-            else {
-                value /= NUMS[i + 1];
-            }
-        }
-        return value;
-    }
-
-    static void rec_func(int k) {
-        if (k == N) { // 다 골랐다!
-            int value = calc();
-            MAX = Math.max(MAX, value);
-            MIN = Math.min(MIN, value);
-        }
-        else {
-            for (int cand = 0; cand < 4; cand++) {
-                if (OPERATORS[cand] >= 1) {
-                    OPERATORS[cand]--;
-                    ORDERS[k] = cand;
-                    rec_func(k + 1);
-                    OPERATORS[cand]++;
-                    ORDERS[k] = 0;
+    static void recFunc(int k) {
+        if (k == N) { // 계산 결과 값 구하기
+            int res = NUMS[1];
+            for (int i = 1; i < N; i++) {
+                if (ORDERS[i] == 1) {
+                    res = res + NUMS[i + 1];
+                }
+                if (ORDERS[i] == 2) {
+                    res = res - NUMS[i + 1];
+                }
+                if (ORDERS[i] == 3) {
+                    res = res * NUMS[i + 1];
+                }
+                if (ORDERS[i] == 4) {
+                    res = res / NUMS[i + 1];
                 }
             }
-        }
-    }
-
-    static int calc2(int operand1, int operator, int operand2) {
-        if (operator == 0) {
-            return operand1 + operand2;
-        }
-        else if (operator == 1) {
-            return operand1 - operand2;
-        }
-        else if (operator == 2) {
-            return operand1 * operand2;
+            MIN = Math.min(res, MIN);
+            MAX = Math.max(res, MAX);
         }
         else {
-            return operand1 / operand2;
-        }
-    }
-
-    static void rec_func2(int k, int value) {
-        if (k == N) { // 다 골랐다!
-            MAX = Math.max(MAX, value);
-            MIN = Math.min(MIN, value);
-        }
-        else {
-            for (int cand = 0; cand < 4; cand++) {
-                if (OPERATORS[cand] >= 1) {
-                    OPERATORS[cand]--;
-                    rec_func2(k + 1, calc2(value, cand, NUMS[k + 1]));
-                    OPERATORS[cand]++;
+            for (int i = 0; i < 4; i++) {
+                if (OPERATORS[i] != 0) {
+                    OPERATORS[i]--;
+                    ORDERS[k] = i + 1; // 1 = +, 2 = -, 3 = *, 4 = /
+                    recFunc(k + 1);
+                    OPERATORS[i]++;
+                    ORDERS[k] = 0;
                 }
             }
         }
