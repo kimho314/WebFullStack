6de662422a97bb7b3e380fdb2c526b6a81376cbf
Hoseop.Kim <kimho314@gmail.com>
2023-07-12 21:25:12 +0900

    add WalkingPark

---
+++ cs-algorithms/WalkingPark.java
@@ -0,0 +1,13 @@
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/172928">...</a>
+ */
+public class WalkingPark {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(String[] park, String[] routes) {
+        int[] answer = {};
+        return answer;
+    }
+}
