0c83ef5dd39908ae7f8dda692597fd2660de6733
Hoseop.Kim <kimho314@gmail.com>
2023-07-16 19:09:53 +0900

    modify SecurityConfig

--- AuthServer/src/main/java/com/example/authserver/config/SecurityConfig.java
+++ AuthServer/src/main/java/com/example/authserver/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.example.authserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
