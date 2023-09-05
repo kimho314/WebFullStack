e3312574f436f36c7bb34ffaf5eafe067b2a7868
Hoseop.Kim <kimho314@gmail.com>
2023-09-05 20:49:53 +0900

    modify BOJ2606

--- cs-algorithms/BOJ2606.java
+++ cs-algorithms/BOJ2606.java
@@ -1,67 +1,46 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class BOJ2606 {
-    static int N;
-    static int M;
-    static int[][] map;
-    static boolean[] visited;
-    static int CNT = 0;
+    static FastReader SC = new FastReader();
+    static int N, M;
+    static ArrayList<Integer>[] MAP;
+    static boolean[] VISITED;
+    static int CNT;
+
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = Integer.parseInt(sc.nextLine());
-        map = new int[N][N];
-        visited = new boolean[N];
-        M = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < M; i++) {
-            String[] split = sc.nextLine().split("");
-            int x = Integer.parseInt(split[0]) - 1;
-            int y = Integer.parseInt(split[1]) - 1;
-            map[x][y] = 1;
-            map[y][x] = 1;
+        N = SC.nextInt();
+        M = SC.nextInt();
+        MAP = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            MAP[i] = new ArrayList<>();
+        }
+        VISITED = new boolean[N + 1];
+        for (int i = 1; i <= M; i++) {
+            int num1 = SC.nextInt();
+            int num2 = SC.nextInt();
+            MAP[num1].add(num2);
+            MAP[num2].add(num1);
         }
 
-        dfs(0);
-        System.out.println(CNT);
-        bfs(0);
         CNT = 0;
-        for (int i = 1; i < N; i++) {
-            if (visited[i]) {
-                CNT++;
-            }
-        }
+        dfs(1);
         System.out.println(CNT);
-
-        sc.close();
     }
 
-    public static void dfs(int start) {
-        visited[start] = true;
+    private static void dfs(int start) {
+        VISITED[start] = true;
+        if (start != 1) {
+            CNT++;
+        }
 
-        for (int y = 0; y < N; y++) {
-            if (!visited[y] && map[start][y] == 1) {
-                CNT++;
-                dfs(y);
+        for (int x : MAP[start]) {
+            if (VISITED[x]) {
+                continue;
             }
+
+            dfs(x);
         }
     }
 
-    public static void bfs(int start) {
-        Queue<Integer> needVisit = new LinkedList<>();
-        needVisit.add(start);
-        visited[start] = true;
-
-        while (!needVisit.isEmpty()) {
-            int x = needVisit.poll();
-
-            for (int y = 0; y < N; y++) {
-                if (!visited[y] && map[x][y] == 1) {
-                    needVisit.add(y);
-                    visited[y] = true;
-                }
-            }
-        }
-    }
 }
