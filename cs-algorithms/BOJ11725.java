6da2f9340424cfe3827bdc0d30805bfe2b135a3c
Hoseop.Kim <kimho314@gmail.com>
2023-09-08 20:49:53 +0900

    modify BOJ11725

--- cs-algorithms/BOJ11725.java
+++ cs-algorithms/BOJ11725.java
@@ -1,43 +1,54 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BOJ11725 {
-    static FastReader sc = new FastReader();
-    static StringBuilder sb = new StringBuilder();
+    static FastReader SC = new FastReader();
     static int N;
     static ArrayList<Integer>[] ADJ;
-    static int[] PARENT;
-
+    static boolean[] VISITED;
+    static int[] ROOT;
 
     public static void main(String[] args) {
-        N = sc.nextInt();
-        PARENT = new int[N + 1];
+        N = SC.nextInt();
+        VISITED = new boolean[N + 1];
+        ROOT = new int[N + 1];
         ADJ = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
             ADJ[i] = new ArrayList<>();
         }
 
-        for (int i = 1; i < N; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            ADJ[x].add(y);
-            ADJ[y].add(x);
+        for (int i = 0; i < N - 1; i++) {
+            int p1 = SC.nextInt();
+            int p2 = SC.nextInt();
+            ADJ[p1].add(p2);
+            ADJ[p2].add(p1);
         }
 
-        dfs(1, -1);
+        bfs(1);
+
         for (int i = 2; i <= N; i++) {
-            sb.append(PARENT[i]).append("\n");
+            System.out.println(ROOT[i]);
         }
-        System.out.println(sb);
     }
 
-    static void dfs(int x, int parent) {
-        for (int y : ADJ[x]) {
-            if (y == parent) {
-                continue;
-            }
+    private static void bfs(int start) {
+        Queue<Integer> needVisit = new LinkedList<>();
+        needVisit.add(start);
 
-            PARENT[y] = x;
-            dfs(y, x);
+        while (!needVisit.isEmpty()) {
+            int x = needVisit.poll();
+            VISITED[x] = true;
+
+            for (int y : ADJ[x]) {
+                if (VISITED[y]) {
+                    continue;
+                }
+
+                VISITED[y] = true;
+                ROOT[y] = x;
+                needVisit.add(y);
+            }
         }
     }
 }
