6fa7e90dbc5c7ac8b278ce3473bc70debb32eb06
Hoseop.Kim <kimho314@gmail.com>
2023-09-18 16:44:00 +0900

    modify BOJ9663

--- cs-algorithms/BOJ9663.java
+++ cs-algorithms/BOJ9663.java
@@ -1,56 +1,47 @@
-import java.util.Scanner;
-
 public class BOJ9663 {
 
     static Integer N, ANS;
 
-    static int[] SELECTED;
-    static Scanner sc = new Scanner(System.in);
+    static int[] COL;
+    static FastReader SC = new FastReader();
 
     public static void main(String[] args) {
-        N = sc.nextInt();
+        N = SC.nextInt();
         ANS = 0;
-        SELECTED = new int[N + 1];
-        rec_func(1);
+        COL = new int[N + 1];
+        recFunc(1);
         System.out.println(ANS);
     }
 
-    static boolean isAvailable(int r1, int c1, int r2, int c2) {
-        if (c1 == c2) {
-            return false;
-        }
-        if (r1 - c1 == r2 - c2) {
-            return false;
-        }
-        if (r1 + c1 == r2 + c2) {
-            return false;
-        }
-        return true;
-    }
-
-    static void rec_func(int row) {
-        if (row == N + 1) { // 다 골랐다!
-            ANS++;
+    private static void recFunc(int row) {
+        if (row == N + 1) {
+            if (validityCheck()) {
+                ANS++;
+            }
         }
         else {
-            for (int cand = 1; cand <= N; cand++) {
-                // row 번째에 cand 가 올 수 있으면
+            for (int c = 1; c <= N; c++) {
+                COL[row] = c;
+                recFunc(row + 1);
+                COL[row] = 0;
+            }
+        }
+    }
 
-                boolean available = true;
-                for (int i = 1; i < row; i++) {
-                    if (!isAvailable(row, cand, i, SELECTED[i])) {
-                        available = false;
-                        break;
-                    }
+    private static boolean validityCheck() {
+        for (int i = 1; i < N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                if (COL[i] == COL[j]) {
+                    return false;
                 }
-
-                if (available) {
-                    SELECTED[row] = cand;
-                    // row+1 번부터 M 번까지 잘 채워주는 함수를 호출해준다.
-                    rec_func(row + 1);
-                    SELECTED[row] = 0;
+                if (COL[i] + i == COL[j] + j) {
+                    return false;
+                }
+                if (COL[i] - i == COL[j] - j) {
+                    return false;
                 }
             }
         }
+        return true;
     }
 }
