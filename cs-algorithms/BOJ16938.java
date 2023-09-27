3dc0741d00e51d4c3167ed0e5c7b77d00414b61f
Hoseop.Kim <kimho314@gmail.com>
2023-09-27 12:15:46 +0900

    modify BOJ16938

--- cs-algorithms/BOJ16938.java
+++ cs-algorithms/BOJ16938.java
@@ -12,19 +12,21 @@ public class BOJ16938 {
         L = SC.nextInt();
         R = SC.nextInt();
         X = SC.nextInt();
-        A = new int[N];
-        for (int i = 0; i < N; i++) {
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
             A[i] = SC.nextInt();
         }
+        Arrays.sort(A);
         for (int i = 2; i <= N; i++) {
             SELECTED = new int[i + 1];
-            recFunc(1, i);
+            recFunc(1, 0, i);
         }
         System.out.println(ANS);
     }
 
-    private static void recFunc(int k, int maxLen) {
+    private static void recFunc(int k, int lastIdx, int maxLen) {
         if (k == maxLen + 1) {
+            System.out.println(Arrays.toString(Arrays.copyOfRange(SELECTED, 1, SELECTED.length)));
             int sum = 0;
             int max = Integer.MIN_VALUE;
             int min = Integer.MAX_VALUE;
@@ -35,18 +37,16 @@ public class BOJ16938 {
             }
             int range = max - min;
             if ((sum >= L && sum <= R) && (range >= X)) {
-                System.out.println(Arrays.toString(Arrays.copyOfRange(SELECTED, 1, SELECTED.length)));
                 ANS++;
             }
         }
         else {
-            for (int i = 0; i < N; i++) {
-                if (A[i] <= SELECTED[k - 1]) {
-                    continue;
+            for (int i = k; i <= N; i++) {
+                if (i > lastIdx) {
+                    SELECTED[k] = A[i];
+                    recFunc(k + 1, i, maxLen);
+                    SELECTED[k] = 0;
                 }
-                SELECTED[k] = A[i];
-                recFunc(k + 1, maxLen);
-                SELECTED[k] = 0;
             }
         }
     }
