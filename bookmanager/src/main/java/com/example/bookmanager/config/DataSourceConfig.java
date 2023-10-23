7de66a51c6c061601ed252c71c3ab4521bdbfc8f
Hoseop.Kim <kimho314@gmail.com>
2023-10-23 22:09:51 +0900

    - add p6spy library - disable jpa auto configuration, enable jpa java configuration

--- bookmanager/src/main/java/com/example/bookmanager/config/DataSourceConfig.java
+++ bookmanager/src/main/java/com/example/bookmanager/config/DataSourceConfig.java
@@ -5,17 +5,20 @@ import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
-//@Configuration
-//@EnableTransactionManagement
+@Configuration
+@EnableTransactionManagement
 public class DataSourceConfig {
 
     @Bean
@@ -36,12 +39,22 @@ public class DataSourceConfig {
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(true);
 
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(vendorAdapter);
         factory.setPackagesToScan("com.example.bookmanager.entity");
         factory.setDataSource(dataSource);
+        factory.setJpaProperties(getJpaProperties());
         return factory;
+    }
+
+    private Properties getJpaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("spring.jpa.hibernate.ddl-auto", "create");
+        properties.setProperty("spring.jpa.open-in-view", "false");
+        properties.setProperty("spring.jpa.hibernate.properties.hibernate.format_sql", "true");
+        return properties;
     }
 
 
