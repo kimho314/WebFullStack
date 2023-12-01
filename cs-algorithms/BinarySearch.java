4ee65fd4f8b2caf7d4e3f927a8f2b270bee92c1f
Hoseop.Kim <kimho314@gmail.com>
2023-12-01 22:09:51 +0900

    modify BinarySearch

--- cs-algorithms/BinarySearch.java
+++ cs-algorithms/BinarySearch.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Objects;
 
 public class BinarySearch {
@@ -8,11 +7,14 @@ public class BinarySearch {
         for (int i = 0; i < 100; i++) {
             list.add((int) (Math.random() * 100));
         }
-        System.out.println(list);
-        list.sort(Comparator.comparing(integer -> integer));
-        System.out.println(list);
+//        System.out.println(list);
+//        list.sort(Comparator.comparing(integer -> integer));
+//        System.out.println(list);
 
-        System.out.println(binarySearch(list, 2));
+//        System.out.println(binarySearch(list, 2));
+
+        int[] arr = new int[]{10, 11, 18, 19, 38, 58, 72, 87, 92};
+        System.out.println(binarySearch(arr, 63)); // index == 5 -> 63이하의 수들 중 가장 큰 값
     }
 
     public static Integer binarySearch(ArrayList<Integer> list, Integer searchItem) {
@@ -29,12 +31,32 @@ public class BinarySearch {
         Integer mid = list.size() / 2;
         if (list.get(mid).equals(searchItem)) {
             return mid;
-        } else {
+        }
+        else {
             if (searchItem < list.get(mid)) {
                 return binarySearch(new ArrayList<>(list.subList(0, mid)), searchItem);
-            } else {
+            }
+            else {
                 return binarySearch(new ArrayList<>(list.subList(mid, list.size())), searchItem);
             }
         }
+    }
+
+    private static int binarySearch(int[] list, int target) {
+        int l = 0;
+        int r = list.length - 1;
+        int res = -1;
+        while (l <= r) {
+            int mid = (r + l) / 2;
+            if (list[mid] < target) {
+                res = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        return res;
     }
 }
