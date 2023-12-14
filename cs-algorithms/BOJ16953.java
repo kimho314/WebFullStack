b035d1725db1043f70ce024549828c4210a8cbe2
Hoseop.Kim <kimho314@gmail.com>
2023-12-14 17:19:52 +0900

    modify BOJ2193

--- cs-algorithms/BOJ16953.java
+++ cs-algorithms/BOJ16953.java
@@ -1,39 +1,41 @@
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class BOJ16953 {
     static FastReader SC = new FastReader();
-    static int A, B;
-    static int[] VISITED;
+    static long A, B;
+    static long RES = -1;
 
     public static void main(String[] args) {
         A = SC.nextInt();
         B = SC.nextInt();
-        VISITED = new int[B + 1];
-        Arrays.fill(VISITED, -1);
 
         bfs();
-        System.out.println(VISITED[B] == -1 ? -1 : VISITED[B] + 1);
+        System.out.println(RES);
     }
 
     private static void bfs() {
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Long> queue = new LinkedList<>();
         queue.add(A);
-        VISITED[A] = 0;
+        queue.add(1L);
 
         while (!queue.isEmpty()) {
-            int n = queue.poll();
-
-            int dn = n * 2;
-            if (dn <= B && VISITED[dn] == -1) {
-                VISITED[dn] = VISITED[n] + 1;
-                queue.add(dn);
+            long n = queue.poll();
+            long cnt = queue.poll();
+            if (n == B) {
+                RES = cnt;
+                break;
             }
-            dn = Integer.parseInt(n + String.valueOf(1));
-            if (dn <= B && VISITED[dn] == -1) {
-                VISITED[dn] = VISITED[n] + 1;
+
+            long dn = n * 2;
+            if (dn <= B) {
                 queue.add(dn);
+                queue.add(cnt + 1);
+            }
+            dn = n * 10 + 1;
+            if (dn <= B) {
+                queue.add(dn);
+                queue.add(cnt + 1);
             }
         }
     }
