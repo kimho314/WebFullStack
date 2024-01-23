206403957ad6f682c8341f25c7bd027fe0351441
Hoseop.Kim <kimho314@gmail.com>
2024-01-23 19:27:41 +0900

    modify BOJ1697

--- cs-algorithms/BOJ1697.java
+++ cs-algorithms/BOJ1697.java
@@ -5,40 +5,49 @@ public class BOJ1697 {
     static FastReader sc = new FastReader();
     static int N, K;
     static int[] DIST;
-    static boolean[] VISIT;
+    static boolean[] VISITED;
 
     public static void main(String[] args) {
         N = sc.nextInt();
         K = sc.nextInt();
-        VISIT = new boolean[100005];
-        DIST = new int[100005];
+        VISITED = new boolean[100001];
+        DIST = new int[100001];
 
         bfs();
         System.out.println(DIST[K]);
     }
 
     static void bfs() {
-        Queue<Integer> needVisit = new LinkedList<>();
-        needVisit.add(N);
-        VISIT[N] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(N);
+        VISITED[N] = true;
         DIST[N] = 0;
 
-        while (!needVisit.isEmpty()) {
-            int x = needVisit.poll();
-            if (x - 1 >= 0 && !VISIT[x - 1]) {
-                VISIT[x - 1] = true;
-                DIST[x - 1] = DIST[x] + 1;
-                needVisit.add(x - 1);
-            }
-            if (x + 1 <= 100000 && !VISIT[x + 1]) {
-                VISIT[x + 1] = true;
-                DIST[x + 1] = DIST[x] + 1;
-                needVisit.add(x + 1);
-            }
-            if (x * 2 <= 100000 && !VISIT[x * 2]) {
-                VISIT[x * 2] = true;
-                DIST[x * 2] = DIST[x] + 1;
-                needVisit.add(x * 2);
+        while (!q.isEmpty()) {
+            int x = q.poll();
+
+            for (int i = 0; i < 3; i++) {
+                int dx = x;
+                if (i == 0) {
+                    dx = 2 * dx;
+                }
+                if (i == 1) {
+                    dx = dx + 1;
+                }
+                if (i == 2) {
+                    dx = dx - 1;
+                }
+
+                if (dx < 0 || dx > 100000) {
+                    continue;
+                }
+                if (VISITED[dx]) {
+                    continue;
+                }
+
+                DIST[dx] = DIST[x] + 1;
+                VISITED[dx] = true;
+                q.add(dx);
             }
         }
     }
