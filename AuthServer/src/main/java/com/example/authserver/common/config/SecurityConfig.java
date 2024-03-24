32827e264514c424c0299e2e02952fac113458f1
Hoseop.Kim <kimho314@gmail.com>
2024-03-24 22:44:29 +0900

    init project

---
+++ AuthServer/src/main/java/com/example/authserver/common/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.authserver.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(Customizer.withDefaults()) // invoke csrfFilter to protect against CSRF attacks
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+        ;
+
+        return http.build();
+    }
+}
