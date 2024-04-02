991354599535e000e59a512603b8ce91b43a7ffa
Hoseop.Kim <kimho314@gmail.com>
2024-04-02 22:22:04 +0900

    - add customizing SpringApplication

--- AuthServer/src/main/java/com/example/authserver/AuthServerApplication.java
+++ AuthServer/src/main/java/com/example/authserver/AuthServerApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServerApplication {
 
     public static void main(String[] args) {
+//        SpringApplication springApplication = new SpringApplication(AuthServerApplication.class);
+//        springApplication.setLazyInitialization(true);
+//        springApplication.run(args);
         SpringApplication.run(AuthServerApplication.class, args);
     }
 
