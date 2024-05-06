3d20ea56990992345da906c8e28a51308d92cc33
Hoseop.Kim <kimho314@gmail.com>
2024-05-06 15:28:24 +0900

    add BOJ2743

---
+++ cs-algorithms/BOJ2743.java
@@ -0,0 +1,8 @@
+public class BOJ2743 {
+    static FastReader SC = new FastReader();
+
+    public static void main(String[] args) {
+        String str = SC.nextLine();
+        System.out.println(str.length());
+    }
+}
