14a7e92c2a415ab90371f00316859688988db0b2
Hoseop.Kim <kimho314@gmail.com>
2024-05-20 21:21:21 +0900

    add Toss2024P1

---
+++ cs-algorithms/Toss20241.java
@@ -0,0 +1,28 @@
+public class Toss2024P1 {
+    public static void main(String[] args) {
+        Toss2024P1 toss = new Toss2024P1();
+        int answer1 = toss.solution(new int[]{30, 30, 60, 90, 60, 15, 15, 60});
+        System.out.println(answer1);
+        int answer2 = toss.solution(new int[]{45, 15, 30});
+        System.out.println(answer2);
+    }
+
+    public int solution(int[] schedules) {
+        int answer = 0;
+
+        int[] dp = new int[schedules.length];
+        dp[0] = schedules[0];
+        dp[1] = schedules[1];
+        dp[2] = schedules[2] + dp[0];
+        for (int i = 3; i < schedules.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + schedules[i], dp[i - 3] + schedules[i]);
+        }
+
+        answer = dp[1];
+        for (int i = 1; i < dp.length; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        return answer;
+    }
+}
