d49297beead2662320eafd7d1b08e580aabb81ad
Hoseop.Kim <kimho314@gmail.com>
2024-06-16 12:10:17 +0900

    feat : add OrderApp

---
+++ hello_spring/src/test/java/com/example/core/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.core.order;
+
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertEquals(1000, order.getDiscountPrice());
+    }
+}
