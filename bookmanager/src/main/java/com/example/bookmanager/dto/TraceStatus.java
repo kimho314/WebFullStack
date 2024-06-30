e669135e236e7630710e16491e0668998f90b37e
Hoseop.Kim <kimho314@gmail.com>
2024-06-30 15:42:30 +0900

    chore : delete bookmanager directory

--- bookmanager/src/main/java/com/example/bookmanager/dto/TraceStatus.java
+++
@@ -1,4 +0,0 @@
-package com.example.bookmanager.dto;
-
-public record TraceStatus(TraceId traceId, Long startTimeMs, String message) {
-}
--- bookmanager/src/main/java/com/example/bookmanager/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookmanager.service;
-
-import com.example.bookmanager.dto.GetMemberDto;
-
-public interface MemberService {
-    GetMemberDto getMember(String userId);
-}
