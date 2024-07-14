bc39b35ed200f49b549f14e47b0b05723cf15920
Hoseop.Kim <kimho314@gmail.com>
2024-07-14 18:28:46 +0900

    feat : modify BOJ12852

--- cs-algorithms/BOJ12852.java
+++ cs-algorithms/BOJ12852.java
@@ -1,45 +1,44 @@
 public class BOJ12852 {
     static FastReader SC = new FastReader();
     static int N;
-    static boolean[] VISITED;
-    static int[] SELECTED;
-    static int MIN = Integer.MAX_VALUE;
-    static String ANS = "";
+    static int[] DP;
+    static int[] TRACE;
 
     public static void main(String[] args) {
+        init();
+        solve();
+    }
+
+    private static void solve() {
+        DP[0] = 0;
+        DP[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            DP[i] = DP[i - 1] + 1;
+            TRACE[i] = i - 1;
+
+            if (i % 2 == 0 && DP[i] > DP[i / 2] + 1) {
+                DP[i] = DP[i / 2] + 1;
+                TRACE[i] = i / 2;
+            }
+            if (i % 3 == 0 && DP[i] > DP[i / 3] + 1) {
+                DP[i] = DP[i / 3] + 1;
+                TRACE[i] = i / 3;
+            }
+        }
+
+        System.out.println(DP[N]);
+        int start = N;
+        while (start > 0) {
+            System.out.print(start + " ");
+            start = TRACE[start];
+        }
+    }
+
+    private static void init() {
         N = SC.nextInt();
-        VISITED = new boolean[N];
-        SELECTED = new int[N];
-
-        dfs(N, 0, 0);
-
-        System.out.println(MIN);
-        System.out.println(ANS);
+        DP = new int[N + 1];
+        TRACE = new int[N + 1];
     }
 
-    private static void dfs(int n, int k, int cnt) {
-        SELECTED[k] = n;
-        if (n <= 1) {
-            if (n == 1 && cnt < MIN) {
-                MIN = cnt;
-                ANS = "";
-                for (int i = 0; i <= k; i++) {
-                    ANS += SELECTED[i] + " ";
-                }
-            }
-        }
-        else {
-            if (n % 3 == 0) {
-                int dn = n / 3;
-                dfs(dn, k + 1, cnt + 1);
-            }
-            if (n % 2 == 0) {
-                int dn = n / 2;
-                dfs(dn, k + 1, cnt + 1);
-            }
-            int dn = n - 1;
-            dfs(dn, k + 1, cnt + 1);
-        }
-        SELECTED[k] = 0;
-    }
 }
