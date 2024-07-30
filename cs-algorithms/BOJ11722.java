37717cdd4f5e92230ec228c614066a014f1fe379
Hoseop.Kim <kimho314@gmail.com>
2024-07-30 23:24:27 +0900

    feat : add BOJ11722

---
+++ cs-algorithms/BOJ11722.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class BOJ11722 {
+    static FastReader SC = new FastReader();
+    static int N;
+    static int[] A, DP;
+
+    public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    private static void solve() {
+        Arrays.fill(DP, 1);
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (A[i] < A[j]) {
+                    DP[i] = Math.max(DP[i], DP[j] + 1);
+                }
+            }
+        }
+
+        int max = DP[0];
+        for (int i = 1; i < N; i++) {
+            max = Math.max(max, DP[i]);
+        }
+        System.out.println(max);
+    }
+
+    private static void input() {
+        N = SC.nextInt();
+        A = new int[N];
+        DP = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = SC.nextInt();
+        }
+    }
+}
