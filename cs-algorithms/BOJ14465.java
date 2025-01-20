df79313bab9167f3d36bf5656e8c0ebe71dda9b2
Hoseop.kim <kimho314@gmail.com>
2025-01-20 21:58:13 +0900

    [feat] : modify BOJ14465

--- cs-algorithms/BOJ14465.java
+++ cs-algorithms/BOJ14465.java
@@ -5,24 +5,40 @@ public class BOJ14465 {
 
     public static void main(String[] args) {
         input();
-        solve();
+//        solve();
+        solve2();
     }
 
-    private static void solve() {
-        int res = Integer.MAX_VALUE;
-        for (int i = 1; i <= N - K + 1; i++) {
-            int cnt = 0;
-            for (int j = i; j < i + K; j++) {
-                if (TRAFFIC[j]) {
-                    cnt++;
-                }
-            }
-            res = Math.min(res, cnt);
+    private static void solve2() {
+        int[] sums = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            sums[i] = sums[i - 1] + (TRAFFIC[i] ? 1 : 0);
         }
 
+        int res = Integer.MAX_VALUE;
+        for (int i = 1; i <= N - K + 1; i++) {
+            int cnt = sums[i + K - 1] - sums[i - 1];
+//            System.out.println(cnt + " " + sums[i + K - 1] + " " + sums[i - 1] + " " + i);
+            res = Math.min(res, cnt);
+        }
         System.out.println(res);
     }
 
+    //    private static void solve() {
+//        int res = Integer.MAX_VALUE;
+//        for (int i = 1; i <= N - K + 1; i++) {
+//            int cnt = 0;
+//            for (int j = i; j < i + K; j++) {
+//                if (TRAFFIC[j]) {
+//                    cnt++;
+//                }
+//            }
+//            res = Math.min(res, cnt);
+//        }
+//
+//        System.out.println(res);
+//    }
+//
     private static void input() {
         N = SC.nextInt();
         K = SC.nextInt();
