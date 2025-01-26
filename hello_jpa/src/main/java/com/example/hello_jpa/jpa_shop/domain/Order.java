5f95083c2d177022288666090135070bc7d7c1d7
Hoseop.kim <kimho314@gmail.com>
2025-01-26 21:19:30 +0900

    [feat] : make jpa relationships among Order, OrderItem, Item

--- hello_jpa/src/main/java/com/example/hello_jpa/jpa_shop/domain/Order.java
+++ hello_jpa/src/main/java/com/example/hello_jpa/jpa_shop/domain/Order.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -17,10 +19,33 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "order_id")
     private Long id;
-    @Column(name = "member_id")
-    private Long memberId;
+//    @Column(name = "member_id")
+//    private Long memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
     private LocalDateTime orderDateTime;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public Order(Member member, LocalDateTime orderDateTime, OrderStatus status) {
+        this(null, member, orderDateTime, status);
+    }
+
+    public Order(Long id, Member member, LocalDateTime orderDateTime, OrderStatus status) {
+        this.id = id;
+        this.member = member;
+        this.orderDateTime = orderDateTime;
+        this.status = status;
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 }
--- hello_jpa/src/main/java/com/example/hello_jpa/jpa_shop/domain/OrderItem.java
+++ hello_jpa/src/main/java/com/example/hello_jpa/jpa_shop/domain/OrderItem.java
@@ -1,6 +1,7 @@
 package com.example.hello_jpa.jpa_shop.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,16 +9,37 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "order_item_id")
     private Long id;
-    @Column(name = "order_id")
-    private Long orderId;
-    @Column(name = "item_id")
-    private Long itemId;
+
+//    @Column(name = "order_id")
+//    private Long orderId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+//    @Column(name = "item_id")
+//    private Long itemId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
     private Integer orderPrice;
     private Integer count;
+
+    public OrderItem(Order order, Integer orderPrice, Integer count) {
+        this(null, order, orderPrice, count);
+    }
+
+    public OrderItem(Long id, Order order, Integer orderPrice, Integer count) {
+        this.id = id;
+        this.order = order;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
 }
