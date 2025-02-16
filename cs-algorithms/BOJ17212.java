b7bec47849e0851c79887852f5e9455e85fe5046
Hoseop.kim <kimho314@gmail.com>
2025-02-16 13:46:30 +0900

    [feat] : add BOJ17212

--- cs-algorithms/BOJ17212.java
+++ cs-algorithms/BOJ17212.java
@@ -8,14 +8,22 @@ public class BOJ17212 {
     }
 
     private static void solve() {
-        int res = 0;
-        int[] coins = {7, 5, 2, 1};
-        for (int i = 0; i < 4; i++) {
-            int cnt = N / coins[i];
-            N -= coins[i] * cnt;
-            res += cnt;
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i >= 2) {
+                dp[i] = Math.min(dp[i], dp[i - 2] + 1);
+            }
+            if (i >= 5) {
+                dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+            }
+            if (i >= 7) {
+                dp[i] = Math.min(dp[i], dp[i - 7] + 1);
+            }
         }
-        System.out.println(res);
+
+        System.out.println(dp[N]);
     }
 
     private static void input() {
