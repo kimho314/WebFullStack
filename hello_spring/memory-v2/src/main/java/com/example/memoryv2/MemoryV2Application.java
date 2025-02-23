4d18838288c8c107617da72b421070fbf231e767
Hoseop.kim <kimho314@gmail.com>
2025-02-24 03:51:08 +0900

    [feat] : add memory-v2 module

---
+++ hello_spring/memory-v2/src/main/java/com/example/memoryv2/MemoryV2Application.java
@@ -0,0 +1,13 @@
+package com.example.memoryv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryV2Application.class, args);
+    }
+
+}
---
+++ hello_spring/memory-v2/src/test/java/com/example/memoryv2/MemoryV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memoryv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
