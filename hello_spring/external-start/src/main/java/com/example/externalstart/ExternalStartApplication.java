25f3e434eb400069a811327cc1cdca22106823c3
Hoseop.kim <kimho314@gmail.com>
2025-02-27 20:39:39 +0900

    [feat] : add external-start module

---
+++ hello_spring/external-start/src/main/java/com/example/externalstart/ExternalStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalStartApplication.class, args);
+    }
+
+}
---
+++ hello_spring/external-start/src/test/java/com/example/externalstart/ExternalStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.externalstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
