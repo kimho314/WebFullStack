50f175bf658635432239d32b8f56a0a69aa7c8c6
Hoseop.kim <kimho314@gmail.com>
2025-03-03 19:27:30 +0900

    [feat] : add summary of chapter 3

---
+++ troubleshooting-java/src/main/java/com/example/troubleshootingjava/Main2.java
@@ -0,0 +1,12 @@
+package com.example.troubleshootingjava;
+
+import java.util.List;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        Decoder d = new Decoder();
+        var result = d.decode(List.of("ab1c", "a112c", "abcd", "1234"));
+        System.out.println(result);
+    }
+}
