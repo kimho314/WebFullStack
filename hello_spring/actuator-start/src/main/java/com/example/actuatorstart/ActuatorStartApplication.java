a2a329c9f24dd173f75fea45fd0bfc6c477f97ea
Hoseop.kim <kimho314@gmail.com>
2025-03-08 19:26:47 +0900

    [feat] : add actuator-start module

---
+++ hello_spring/actuator-start/src/main/java/com/example/actuatorstart/ActuatorStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.actuatorstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorStartApplication.class, args);
+    }
+
+}
---
+++ hello_spring/actuator-start/src/test/java/com/example/actuatorstart/ActuatorStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actuatorstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
