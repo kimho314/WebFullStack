4dea44f7118f70cddb2380473aa24d27e215ec7b
Hoseop.kim <kimho314@gmail.com>
2025-03-11 21:43:03 +0900

    [feat] : add httpExchangeRepository

--- hello_spring/actuator-start/src/main/java/com/example/actuatorstart/ActuatorStartApplication.java
+++ hello_spring/actuator-start/src/main/java/com/example/actuatorstart/ActuatorStartApplication.java
@@ -1,7 +1,9 @@
 package com.example.actuatorstart;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorStartApplication {
@@ -10,4 +12,9 @@ public class ActuatorStartApplication {
         SpringApplication.run(ActuatorStartApplication.class, args);
     }
 
+
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
