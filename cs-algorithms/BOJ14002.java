b729ee3b93d937c5a0a8dc26efd0fbf8337e7f25
Hoseop.kim <kimho314@gmail.com>
2025-03-30 21:36:27 +0900

    [feat] : modify BOJ14002

--- cs-algorithms/BOJ14002.java
+++ cs-algorithms/BOJ14002.java
@@ -1,6 +1,4 @@
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
 
 public class BOJ14002 {
     private static FastReader SC = new FastReader();
@@ -15,44 +13,35 @@ public class BOJ14002 {
     private static void solve() {
         int[] dp = new int[N];
         Arrays.fill(dp, 1);
-        LinkedList<Integer>[] list = new LinkedList[N];
+        String[] list = new String[N];
         for (int i = 0; i < N; i++) {
-            list[i] = new LinkedList<>();
-            list[i].add(A[i]);
+            list[i] = A[i] + "";
         }
 
+
         for (int i = 1; i < N; i++) {
-            int maxIdx = i;
             for (int j = 0; j < i; j++) {
                 if (A[j] < A[i]) {
-                    if (dp[i] < dp[j] + 1) {
-                        maxIdx = j;
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        String tmp = list[j] + " " + A[i];
+                        list[i] = tmp;
                     }
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
-            }
-
-//            System.out.println(i + " " + maxIdx);
-            if (maxIdx < i) {
-                list[i].addAll(list[maxIdx]);
             }
         }
 
         int max = 0;
-        int maxIdx = 0;
+        int maxIdx = -1;
         for (int i = 0; i < N; i++) {
-            if (max < dp[i]) {
+            if (dp[i] > max) {
+                max = dp[i];
                 maxIdx = i;
             }
-            max = Math.max(max, dp[i]);
         }
+
         System.out.println(max);
-        StringBuilder sb = new StringBuilder();
-        Iterator<Integer> iter = list[maxIdx].descendingIterator();
-        while (iter.hasNext()) {
-            sb.append(iter.next()).append(" ");
-        }
-        System.out.println(sb);
+        System.out.println(list[maxIdx]);
     }
 
     private static void input() {
