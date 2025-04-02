f34770f6e7d9976f043a19113cba8ffe5865b4cb
Hoseop.kim <kimho314@gmail.com>
2025-04-02 22:40:04 +0900

    [feat] : add BOJ1912

--- cs-algorithms/BOJ1912.java
+++ cs-algorithms/BOJ1912.java
@@ -1,25 +1,36 @@
 public class BOJ1912 {
-    static FastReader SC = new FastReader();
-    static int N;
-    static int[] A, DP;
+    private static FastReader SC = new FastReader();
+    private static int N;
+    private static int[] A;
 
     public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    private static void solve() {
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            dp[i] = A[i];
+        }
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = Math.max(dp[i], dp[i - 1] + A[i]);
+        }
+
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        System.out.println(res);
+    }
+
+    private static void input() {
         N = SC.nextInt();
         A = new int[N];
         for (int i = 0; i < N; i++) {
             A[i] = SC.nextInt();
         }
-
-        DP = new int[N];
-        DP[0] = A[0];
-        for (int i = 1; i < N; i++) {
-            DP[i] = Math.max(DP[i - 1] + A[i], A[i]);
-        }
-
-        int res = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++) {
-            res = Math.max(res, DP[i]);
-        }
-        System.out.println(res);
     }
+
 }
