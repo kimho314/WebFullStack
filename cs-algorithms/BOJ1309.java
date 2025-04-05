1fc9a07b8a3bdf7b4f407a6fd09ad68c52bee02b
Hoseop.kim <kimho314@gmail.com>
2025-04-05 16:38:01 +0900

    [feat] : modify BOJ1309

--- cs-algorithms/BOJ1309.java
+++ cs-algorithms/BOJ1309.java
@@ -1,32 +1,27 @@
 public class BOJ1309 {
-    static FastReader SC = new FastReader();
-    static int N;
-    static long[][] DP;
+    private static FastReader SC = new FastReader();
+    private static int N;
 
     public static void main(String[] args) {
-        init();
+        input();
         solve();
     }
 
     private static void solve() {
-        DP[1][0] = 1;
-        DP[1][1] = 1;
-        DP[1][2] = 1;
-        DP[2][0] = 3;
-        DP[2][1] = 2;
-        DP[2][2] = 2;
-        for (int i = 3; i <= 100_000; i++) {
-            DP[i][0] = (DP[i - 1][0] + DP[i - 1][1] + DP[i - 1][2]) % 9901;
-            DP[i][1] = (DP[i - 1][0] + DP[i - 1][2]) % 9901;
-            DP[i][2] = (DP[i - 1][0] + DP[i - 1][1]) % 9901;
+        long[][] dp = new long[100_000 + 1][3];
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % 9901;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
         }
 
-        long res = (DP[N][0] + DP[N][1] + DP[N][2]) % 9901;
-        System.out.println(res);
+        System.out.println((dp[N][0] + dp[N][1] + dp[N][2]) % 9901);
     }
 
-    private static void init() {
+    private static void input() {
         N = SC.nextInt();
-        DP = new long[100_000 + 1][3];
     }
 }
