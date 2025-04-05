111f41236af45c7d3a0d7aa9f018b1d8cc501b3a
Hoseop.kim <kimho314@gmail.com>
2025-04-05 23:15:52 +0900

    [feat] : modify BOJ9465

--- cs-algorithms/BOJ9465.java
+++ cs-algorithms/BOJ9465.java
@@ -7,39 +7,32 @@ public class BOJ9465 {
         input();
         while (T > 0) {
             T--;
-            solve();
+            int n = SC.nextInt();
+            int[][] arr = new int[n + 1][2];
+            for (int i = 0; i < 2; i++) {
+                for (int j = 1; j <= n; j++) {
+                    arr[j][i] = SC.nextInt();
+                }
+            }
+            solve(n, arr);
         }
+
         System.out.println(SB);
     }
 
-    private static void solve() {
-        int n = SC.nextInt();
-        int[][] cost = new int[2][n];
-        int[][] dp = new int[2][n];
-
-        String[] st1 = SC.nextLine().split(" ");
-        String[] st2 = SC.nextLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            cost[0][i] = Integer.parseInt(st1[i]);
-            cost[1][i] = Integer.parseInt(st2[i]);
+    private static void solve(int n, int[][] arr) {
+        int[][] dp = new int[n + 1][2];
+        dp[1][0] = arr[1][0];
+        dp[1][1] = arr[1][1];
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][1], Math.max(dp[i - 2][0], dp[i - 2][1])) + arr[i][0];
+            dp[i][1] = Math.max(dp[i - 1][0], Math.max(dp[i - 2][0], dp[i - 2][1])) + arr[i][1];
         }
 
-        dp[0][0] = cost[0][0];
-        dp[1][0] = cost[1][0];
-        int max = Math.max(dp[0][0], dp[1][0]);
-        for (int i = 1; i < n; i++) {
-            if (i == 1) {
-                dp[0][i] = dp[1][0] + cost[0][1];
-                dp[1][i] = dp[0][0] + cost[1][1];
-                max = Math.max(dp[0][i], dp[1][i]);
-                continue;
-            }
-            dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + cost[0][i];
-            dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + cost[1][i];
-            max = Math.max(max, Math.max(dp[0][i], dp[1][i]));
-        }
-        SB.append(max).append("\n");
+        int res = Math.max(dp[n][0], dp[n][1]);
+        SB.append(res).append("\n");
     }
+
 
     private static void input() {
         T = SC.nextInt();
