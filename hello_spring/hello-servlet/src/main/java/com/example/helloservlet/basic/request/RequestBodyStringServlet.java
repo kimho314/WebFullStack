912d173b977d28c299dba25952d0fb1f447989d8
Hoseop.kim <kimho314@gmail.com>
2025-04-12 13:56:07 +0900

    [feat] : add RequestBodyStringServlet

---
+++ hello_spring/hello-servlet/src/main/java/com/example/helloservlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,27 @@
+package com.example.helloservlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StreamUtils;
+
+@Slf4j
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletInputStream inputStream = req.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody = {}", messageBody);
+
+        resp.getWriter().write("ok");
+    }
+}
