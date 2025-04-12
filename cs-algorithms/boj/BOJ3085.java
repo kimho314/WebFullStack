dd54b1b9e50d8c3d976b5c04a7cc0ab4e89286a4
Hoseop.kim <kimho314@gmail.com>
2025-04-12 19:32:59 +0900

    [feat] : modify BOJ3085

--- cs-algorithms/boj/BOJ3085.java
+++ cs-algorithms/boj/BOJ3085.java
@@ -1,80 +1,84 @@
 package boj;
 
 public class BOJ3085 {
-    static FastReader SC = new FastReader();
-    static int N;
+    private static FastReader SC = new FastReader();
+    private static int N;
+    private static char[][] MAP;
 
     public static void main(String[] args) {
-        N = SC.nextInt();
-        char[][] map = new char[N][N];
-        for (int i = 0; i < N; i++) {
-            char[] arr = SC.nextLine().toCharArray();
-            for (int j = 0; j < N; j++) {
-                map[i][j] = arr[j];
-            }
-        }
+        input();
+        solve();
+    }
 
-        int res = 0;
+    private static void solve() {
+        int res = check();
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (i + 1 < N && map[i][j] != map[i + 1][j]) {
-                    char temp = map[i][j];
-                    map[i][j] = map[i + 1][j];
-                    map[i + 1][j] = temp;
-
-                    int cnt = check(map);
+                if (j < N - 1 && MAP[i][j] != MAP[i][j + 1]) {
+                    swap(i, j, i, j + 1);
+                    int cnt = check();
                     res = Math.max(res, cnt);
-
-                    temp = map[i][j];
-                    map[i][j] = map[i + 1][j];
-                    map[i + 1][j] = temp;
+                    swap(i, j, i, j + 1);
                 }
 
-                if (j + 1 < N && map[i][j] != map[i][j + 1]) {
-                    char temp = map[i][j];
-                    map[i][j] = map[i][j + 1];
-                    map[i][j + 1] = temp;
-
-                    int cnt = check(map);
+                if (i < N - 1 && MAP[i][j] != MAP[i + 1][j]) {
+                    swap(i, j, i + 1, j);
+                    int cnt = check();
                     res = Math.max(res, cnt);
-
-                    temp = map[i][j];
-                    map[i][j] = map[i][j + 1];
-                    map[i][j + 1] = temp;
+                    swap(i, j, i + 1, j);
                 }
             }
         }
+
         System.out.println(res);
     }
 
-    private static int check(char[][] arr) {
-        int ans = 1;
+    private static void swap(int y1, int x1, int y2, int x2) {
+        char temp = MAP[y2][x2];
+        MAP[y2][x2] = MAP[y1][x1];
+        MAP[y1][x1] = temp;
+    }
+
+    private static int check() {
+        int res = 0;
         for (int i = 0; i < N; i++) {
             int cnt = 1;
             for (int j = 0; j < N - 1; j++) {
-                if (arr[i][j] == arr[i][j + 1]) {
-                    ++cnt;
+                if (MAP[i][j] == MAP[i][j + 1]) {
+                    cnt++;
                 }
                 else {
                     cnt = 1;
                 }
 
-                ans = Math.max(cnt, ans);
+                res = Math.max(cnt, res);
             }
 
             cnt = 1;
             for (int j = 0; j < N - 1; j++) {
-                if (arr[j][i] == arr[j + 1][i]) {
-                    ++cnt;
+                if (MAP[j][i] == MAP[j + 1][i]) {
+                    cnt++;
                 }
                 else {
                     cnt = 1;
                 }
 
-                ans = Math.max(cnt, ans);
+                res = Math.max(res, cnt);
             }
         }
 
-        return ans;
+        return res;
+    }
+
+    private static void input() {
+        N = SC.nextInt();
+        MAP = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            String str = SC.nextLine();
+            for (int j = 0; j < str.length(); j++) {
+                MAP[i][j] = str.charAt(j);
+            }
+        }
     }
 }
