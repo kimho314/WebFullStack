ab5ce534800d649dc2626b8b43393361fef2b4e7
Hoseop.kim <kimho314@gmail.com>
2025-04-19 15:03:45 +0900

    [feat] : modify BOJ2606

--- cs-algorithms/boj/BOJ2606.java
+++ cs-algorithms/boj/BOJ2606.java
@@ -3,46 +3,46 @@ package boj;
 import java.util.ArrayList;
 
 public class BOJ2606 {
-    static FastReader SC = new FastReader();
-    static int N, M;
-    static ArrayList<Integer>[] MAP;
-    static boolean[] VISITED;
-    static int CNT;
-
+    private static FastReader SC = new FastReader();
+    private static int N, M;
+    private static ArrayList<Integer>[] ADJ;
+    private static boolean[] VISITED;
+    private static int CNT;
 
     public static void main(String[] args) {
-        N = SC.nextInt();
-        M = SC.nextInt();
-        MAP = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) {
-            MAP[i] = new ArrayList<>();
-        }
-        VISITED = new boolean[N + 1];
-        for (int i = 1; i <= M; i++) {
-            int num1 = SC.nextInt();
-            int num2 = SC.nextInt();
-            MAP[num1].add(num2);
-            MAP[num2].add(num1);
-        }
+        input();
+        solve();
+    }
 
-        CNT = 0;
+    private static void solve() {
         dfs(1);
         System.out.println(CNT);
     }
 
-    private static void dfs(int start) {
-        VISITED[start] = true;
-        if (start != 1) {
-            CNT++;
-        }
-
-        for (int x : MAP[start]) {
-            if (VISITED[x]) {
-                continue;
+    private static void dfs(int x) {
+        VISITED[x] = true;
+        for (int n : ADJ[x]) {
+            if (!VISITED[n]) {
+                CNT++;
+                dfs(n);
             }
-
-            dfs(x);
         }
     }
 
+    private static void input() {
+        N = SC.nextInt();
+        M = SC.nextInt();
+        ADJ = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            ADJ[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < M; i++) {
+            int n1 = SC.nextInt();
+            int n2 = SC.nextInt();
+            ADJ[n1].add(n2);
+            ADJ[n2].add(n1);
+        }
+        VISITED = new boolean[N + 1];
+        CNT = 0;
+    }
 }
