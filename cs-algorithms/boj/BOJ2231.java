2745593fc79551a8ff74aa6a48599563d1e3d12d
Hoseop.kim <kimho314@gmail.com>
2025-04-19 11:34:32 +0900

    [feat] : modify BOJ2231

--- cs-algorithms/boj/BOJ2231.java
+++ cs-algorithms/boj/BOJ2231.java
@@ -1,27 +1,38 @@
 package boj;
 
 public class BOJ2231 {
-    static FastReader SC = new FastReader();
-    static int N;
+    private static FastReader SC = new FastReader();
+    private static int N;
 
     public static void main(String[] args) {
-        N = SC.nextInt();
+        input();
+        solve();
+    }
 
+    private static void solve() {
         int res = 0;
         for (int i = 1; i < N; i++) {
-            int sum = i;
-            String num = String.valueOf(i);
-            String[] split = num.split("");
-            for (String s : split) {
-                sum += Integer.parseInt(s);
-            }
-
-            if (sum == N) {
+            int num = getNum(i);
+            // System.out.println(i + " " + num);
+            if (num == N) {
                 res = i;
                 break;
             }
         }
-
         System.out.println(res);
+    }
+
+    private static int getNum(int n) {
+        int res = n;
+        String str = String.valueOf(n);
+        for (int i = 0; i < str.length(); i++) {
+            int tmp = Integer.parseInt(String.valueOf(str.charAt(i)));
+            res += tmp;
+        }
+        return res;
+    }
+
+    private static void input() {
+        N = SC.nextInt();
     }
 }
