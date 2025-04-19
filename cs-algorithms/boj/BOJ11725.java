b0d3158a55e5c6dfb3100134fb1ba2ef359cf436
Hoseop.kim <kimho314@gmail.com>
2025-04-19 15:46:21 +0900

    [feat] : modify BOJ11725

--- cs-algorithms/boj/BOJ11725.java
+++ cs-algorithms/boj/BOJ11725.java
@@ -1,47 +1,51 @@
 package boj;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class BOJ11725 {
-    static FastReader SC = new FastReader();
-    static int N;
-    static ArrayList<Integer>[] ADJ;
-    static boolean[] VISITED;
-    static int[] ROOT;
+    private static FastReader SC = new FastReader();
+    private static int N;
+    private static ArrayList<Integer>[] ADJ;
+    private static boolean[] VISITED;
+    private static int[] PARENTS;
 
     public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    private static void solve() {
+        dfs(1);
+
+        for (int i = 2; i <= N; i++) {
+            System.out.println(PARENTS[i]);
+        }
+    }
+
+    private static void dfs(int x) {
+        VISITED[x] = true;
+
+        for (int n : ADJ[x]) {
+            if (!VISITED[n]) {
+                PARENTS[n] = x;
+                dfs(n);
+            }
+        }
+    }
+
+    private static void input() {
         N = SC.nextInt();
-        VISITED = new boolean[N + 1];
-        ROOT = new int[N + 1];
         ADJ = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
             ADJ[i] = new ArrayList<>();
         }
-
+        VISITED = new boolean[N + 1];
+        PARENTS = new int[N + 1];
         for (int i = 0; i < N - 1; i++) {
-            int p1 = SC.nextInt();
-            int p2 = SC.nextInt();
-            ADJ[p1].add(p2);
-            ADJ[p2].add(p1);
-        }
-
-        dfs(1, 1);
-        for (int i = 2; i <= N; i++) {
-            System.out.println(ROOT[i]);
-        }
-    }
-
-    private static void dfs(int start, int parent) {
-        VISITED[start] = true;
-        ROOT[start] = parent;
-
-        for (int x : ADJ[start]) {
-            if (VISITED[x]) {
-                continue;
-            }
-
-            VISITED[x] = true;
-            dfs(x, start);
+            int n1 = SC.nextInt();
+            int n2 = SC.nextInt();
+            ADJ[n1].add(n2);
+            ADJ[n2].add(n1);
         }
     }
 }
