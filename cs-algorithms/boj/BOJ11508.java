b8c2ec9a7e714669836d7d188bd2bfb6015706d9
Hoseop.kim <kimho314@gmail.com>
2025-04-21 02:26:46 +0900

    [feat] : modify BOJ11508

--- cs-algorithms/boj/BOJ11508.java
+++ cs-algorithms/boj/BOJ11508.java
@@ -1,42 +1,35 @@
 package boj;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class BOJ11508 {
-    static FastReader SC = new FastReader();
-    static int N;
-    static int[] C;
+    private static FastReader SC = new FastReader();
+    private static int N;
+    private static int[] C;
 
     public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    private static void solve() {
+        Arrays.sort(C);
+
+        int res = 0;
+        for (int i = N - 1; i >= 0; i -= 3) {
+            res += C[i];
+            if (i - 1 >= 0) {
+                res += C[i - 1];
+            }
+        }
+        System.out.println(res);
+    }
+
+    private static void input() {
         N = SC.nextInt();
         C = new int[N];
         for (int i = 0; i < N; i++) {
             C[i] = SC.nextInt();
         }
-
-
-        long sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += C[i];
-        }
-
-        int[] sortedArr = Arrays.stream(C).boxed()
-                .sorted(Collections.reverseOrder())
-                .mapToInt(Integer::intValue)
-                .toArray();
-
-        // 3 3 2 2
-        // 0 1 2 3
-
-        // 6 5 5 5 5 3
-        // 0 1 2 3 4 5
-
-        // 10 9 4 4 3 2
-        // 0  1 2 3 4 5
-        for (int i = 2; i < N; i += 3) {
-            sum -= sortedArr[i];
-        }
-        System.out.println(sum);
     }
 }
