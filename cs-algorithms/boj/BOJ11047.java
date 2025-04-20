55d5e4f7dd560076fb9a01e55413f1a302b3a795
Hoseop.kim <kimho314@gmail.com>
2025-04-21 02:01:13 +0900

    [feat] : modify BOJ11047

--- cs-algorithms/boj/BOJ11047.java
+++ cs-algorithms/boj/BOJ11047.java
@@ -1,30 +1,31 @@
 package boj;
 
 public class BOJ11047 {
-    static FastReader SC = new FastReader();
-    static int N, K;
-
-    static int[] A;
+    private static FastReader SC = new FastReader();
+    private static int N, K;
+    private static int[] A;
 
     public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    private static void solve() {
+        int res = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            int cnt = K / A[i];
+            res += cnt;
+            K -= (cnt * A[i]);
+        }
+        System.out.println(res);
+    }
+
+    private static void input() {
         N = SC.nextInt();
         K = SC.nextInt();
         A = new int[N];
         for (int i = 0; i < N; i++) {
             A[i] = SC.nextInt();
         }
-
-        int cnt = 0;
-        for (int i = A.length - 1; i >= 0; i--) {
-            if (A[i] > K) {
-                continue;
-            }
-
-            int tmpCnt = K / A[i];
-            cnt += tmpCnt;
-            K %= (A[i] * tmpCnt);
-        }
-
-        System.out.println(cnt);
     }
 }
