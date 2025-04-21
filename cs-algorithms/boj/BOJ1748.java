2023981eaa7ecaa396a9940f82b6698dbf3e19ea
Hoseop.kim <kimho314@gmail.com>
2025-04-22 07:05:49 +0900

    [feat] : modify BOJ1748

--- cs-algorithms/boj/BOJ1748.java
+++ cs-algorithms/boj/BOJ1748.java
@@ -1,42 +1,36 @@
 package boj;
 
+import java.util.*;
+
 public class BOJ1748 {
-    static FastReader SC = new FastReader();
-    static int N;
+    private static FastReader SC = new FastReader();
+    private static int N;
 
     public static void main(String[] args) {
-        N = SC.nextInt();
-        int res = 0;
-        for (int i = 1; i <= N; i++) {
-            if (i >= 1 && i < 10) {
-                res += 1;
-            }
-            else if (i >= 10 && i < 100) {
-                res += 2;
-            }
-            else if (i >= 100 && i < 1000) {
-                res += 3;
-            }
-            else if (i >= 1000 && i < 10000) {
-                res += 4;
-            }
-            else if (i >= 10000 && i < 100000) {
-                res += 5;
-            }
-            else if (i >= 100000 && i < 1_000_000) {
-                res += 6;
-            }
-            else if (i >= 1_000_000 && i < 10_000_000) {
-                res += 7;
-            }
-            else if (i >= 10_000_000 && i < 100_000_000) {
-                res += 8;
-            }
-            else if (i == 100_000_000) {
-                res += 9;
-            }
+        input();
+        solve();
+    }
+
+    private static void solve() {
+        int len = String.valueOf(N).length();
+        int start = 1;
+        int end = 0;
+        long res = 0;
+
+        for (int i = 1; i < len; i++) {
+            start = (int) Math.pow(10, i - 1);
+            end = start * 10 - 1;
+            res += (long) (end - start + 1) * i;
         }
 
+        start = (int) Math.pow(10, len - 1);
+        end = Math.min(N, start * 10 - 1);
+        res += (long) (end - start + 1) * len;
+
         System.out.println(res);
+    }
+
+    private static void input() {
+        N = SC.nextInt();
     }
 }
