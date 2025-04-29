eff9dbc3f9ed2936fb49173feff88c3c0e89608a
Hoseop.kim <kimho314@gmail.com>
2025-04-29 22:45:20 +0900

    [feat]: Implement FrontController V3 with JSP-based views

--- hello_spring/hello-servlet/src/main/java/com/example/helloservlet/web/frontcontroller/MyView.java
+++ hello_spring/hello-servlet/src/main/java/com/example/helloservlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -22,5 +23,19 @@ public class MyView {
         throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(
+        Map<String, Object> model,
+        HttpServletRequest req,
+        HttpServletResponse resp
+    ) throws ServletException, IOException {
+        modelToRequestAttribute(model, req);
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest req) {
+        model.forEach(req::setAttribute);
     }
 }
