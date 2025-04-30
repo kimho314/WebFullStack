f911246c83c330ed851cfdbf9f7f15005bb7b5e5
Hoseop.kim <kimho314@gmail.com>
2025-04-30 10:12:48 +0900

    [feat]: BOJ13305

--- cs-algorithms/boj/BOJ13305.java
+++ cs-algorithms/boj/BOJ13305.java
@@ -1,29 +1,39 @@
 package boj;
 
 public class BOJ13305 {
-    static FastReader SC = new FastReader();
-    static int N;
-    static int[] PRICE, DIST;
+    private static FastReader SC = new FastReader();
+    private static int N;
+    private static long[] G;
+    private static long[] D;
 
     public static void main(String[] args) {
-        N = SC.nextInt();
-        DIST = new int[N - 1];
-        PRICE = new int[N];
-        for (int i = 0; i < N - 1; i++) {
-            DIST[i] = SC.nextInt();
-        }
-        for (int i = 0; i < N; i++) {
-            PRICE[i] = SC.nextInt();
+        input();
+        solve();
+    }
+
+    private static void solve() {
+        long res = G[0] * D[0];
+        long min = G[0];
+
+        for (int i = 1; i < N - 1; i++) {
+            if (G[i] < min) {
+                min = G[i];
+            }
+            res += min * D[i];
         }
 
-        long res = 0;
-        int cheap = PRICE[0];
-        for (int i = 0; i < N - 1; i++) {
-            if (PRICE[i] < cheap) {
-                cheap = PRICE[i];
-            }
-            res += (long) cheap * (long) DIST[i];
-        }
         System.out.println(res);
+    }
+
+    private static void input() {
+        N = SC.nextInt();
+        D = new long[N - 1];
+        for (int i = 0; i < N - 1; i++) {
+            D[i] = SC.nextInt();
+        }
+        G = new long[N];
+        for (int i = 0; i < N; i++) {
+            G[i] = SC.nextInt();
+        }
     }
 }
