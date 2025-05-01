ca6495643a7d6fb442a8a72fa3248045ce4cc134
Hoseop.kim <kimho314@gmail.com>
2025-05-01 18:14:02 +0900

    [feat]: modify BOJ5568

--- cs-algorithms/boj/BOJ5568.java
+++ cs-algorithms/boj/BOJ5568.java
@@ -1,49 +1,49 @@
 package boj;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class BOJ5568 {
-    static FastReader SC = new FastReader();
-    static int N, K;
-    static int[] SELECTED;
-    static int[] NUMS = new int[100];
-    static Set<String> set = new HashSet<>();
+    private static FastReader SC = new FastReader();
+    private static int N, K;
+    private static int[] A, CNT;
 
     public static void main(String[] args) {
-        N = SC.nextInt();
-        K = SC.nextInt();
-        for (int i = 0; i < N; i++) {
-            int input = SC.nextInt();
-            NUMS[input]++;
-        }
-        SELECTED = new int[K + 1];
+        input();
+        solve();
+    }
 
-        recFunc(1, K);
+    private static void solve() {
+        HashSet<Integer> set = new HashSet<>();
+
+        recFunc("", 0, set);
+
         System.out.println(set.size());
     }
 
-    private static void recFunc(int k, int len) {
-        if (k == len + 1) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 1; i <= K; i++) {
-                sb.append(SELECTED[i]);
-            }
-//            System.out.println(sb.toString());
-            set.add(sb.toString());
-        }
-        else {
-            for (int i = 1; i <= 99; i++) {
-                if (NUMS[i] <= 0) {
-                    continue;
+    private static void recFunc(String str, int cnt, HashSet<Integer> set) {
+        if (cnt == K) {
+            int num = Integer.parseInt(str);
+            // System.out.println(num);
+            set.add(num);
+        } else {
+            for (int i = 0; i < N; i++) {
+                if (CNT[A[i]] > 0) {
+                    CNT[A[i]]--;
+                    recFunc(str + A[i], cnt + 1, set);
+                    CNT[A[i]]++;
                 }
-
-                SELECTED[k] = i;
-                NUMS[i]--;
-                recFunc(k + 1, len);
-                SELECTED[k] = 0;
-                NUMS[i]++;
             }
+        }
+    }
+
+    private static void input() {
+        N = SC.nextInt();
+        K = SC.nextInt();
+        A = new int[N];
+        CNT = new int[100];
+        for (int i = 0; i < N; i++) {
+            A[i] = SC.nextInt();
+            CNT[A[i]]++;
         }
     }
 }
