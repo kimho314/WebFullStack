a271c9ce11f25cd0f22332c1d871ba6ccedae4cd
Hoseop.kim <kimho314@gmail.com>
2025-05-01 14:05:23 +0900

    [feat]: modify BOJ21937

--- cs-algorithms/boj/BOJ21937.java
+++ cs-algorithms/boj/BOJ21937.java
@@ -1,13 +1,13 @@
 package boj;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class BOJ21937 {
     private static FastReader SC = new FastReader();
     private static int N, M, X;
     private static ArrayList<Integer>[] ADJ;
     private static boolean[] VISITED;
-    private static int CNT = 0;
+    private static int CNT;
 
     public static void main(String[] args) {
         input();
@@ -16,33 +16,35 @@ public class BOJ21937 {
 
     private static void solve() {
         dfs(X);
-        System.out.println(CNT);
+
+        System.out.println(CNT - 1);
     }
 
     private static void dfs(int x) {
         VISITED[x] = true;
-        for (Integer n : ADJ[x]) {
+        CNT++;
+
+        for (int n : ADJ[x]) {
             if (!VISITED[n]) {
-                CNT++;
                 dfs(n);
             }
         }
     }
 
-
     private static void input() {
         N = SC.nextInt();
         M = SC.nextInt();
         ADJ = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             ADJ[i] = new ArrayList<>();
         }
+        VISITED = new boolean[N + 1];
         for (int i = 0; i < M; i++) {
-            int a = SC.nextInt();
-            int b = SC.nextInt();
-            ADJ[b].add(a);
+            int n1 = SC.nextInt();
+            int n2 = SC.nextInt();
+            ADJ[n2].add(n1);
         }
         X = SC.nextInt();
-        VISITED = new boolean[N + 1];
+        CNT = 0;
     }
 }
