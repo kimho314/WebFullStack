7d63f5e246f02f9a43503a393bf6f7031e68d429
Hoseop.kim <kimho314@gmail.com>
2025-05-03 15:27:53 +0900

    [feat]: Add hello-springmvc module with basic Spring MVC setup
    Introduces the hello-springmvc module to the project with a basic Spring Boot configuration, dependencies, and example controllers. Includes Gradle setup, logging, and static resources to support initial application development.

---
+++ hello_spring/hello-springmvc/src/main/java/com/example/hellospringmvc/HelloSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringmvcApplication.class, args);
+    }
+
+}
---
+++ hello_spring/hello-springmvc/src/test/java/com/example/hellospringmvc/HelloSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
