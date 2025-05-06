4cbeee77ce583176f249a0e227107f9437c1f7a5
Hoseop.kim <kimho314@gmail.com>
2025-05-06 17:55:41 +0900

    [feat]: modify BOJ14501

--- cs-algorithms/boj/BOJ14501.java
+++ cs-algorithms/boj/BOJ14501.java
@@ -1,27 +1,41 @@
 package boj;
 
+import java.util.*;
+
 public class BOJ14501 {
-    static FastReader SC = new FastReader();
-    static int N;
-    static int[] T, P;
-    static int[] DP;
+    private static FastReader SC = new FastReader();
+    private static int N;
+    private static int[][] A;
 
     public static void main(String[] args) {
-        N = SC.nextInt();
-        T = new int[N];
-        P = new int[N];
-        for (int i = 0; i < N; i++) {
-            T[i] = SC.nextInt();
-            P[i] = SC.nextInt();
+        input();
+        solve();
+    }
+
+    private static void solve() {
+        int[] dp = new int[N + 2];
+
+        for (int i = 1; i <= N + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                if (j + A[j][0] <= i) {
+                    dp[i] = Math.max(dp[i], dp[j] + A[j][1]);
+                }
+            }
         }
 
-        DP = new int[N + 1];
-        for (int i = 0; i < N; i++) {
-            if (i + T[i] <= N) {
-                DP[i + T[i]] = Math.max(DP[i + T[i]], DP[i] + P[i]);
-            }
-            DP[i + 1] = Math.max(DP[i + 1], DP[i]);
+        System.out.println(dp[N + 1]);
+    }
+
+
+
+    private static void input() {
+        N = SC.nextInt();
+        A = new int[N + 2][2];
+        for (int i = 1; i <= N; i++) {
+            int t = SC.nextInt();
+            int p = SC.nextInt();
+            A[i][0] = t;
+            A[i][1] = p;
         }
-        System.out.println(DP[N]);
     }
 }
