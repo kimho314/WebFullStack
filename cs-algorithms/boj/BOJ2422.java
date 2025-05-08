345a7a4ec08b53eed9ddc9f03486f79172d50509
Hoseop.kim <kimho314@gmail.com>
2025-05-08 23:50:10 +0900

    [feat]: modify BOJ2422

--- cs-algorithms/boj/BOJ2422.java
+++ cs-algorithms/boj/BOJ2422.java
@@ -5,8 +5,7 @@ import java.util.*;
 public class BOJ2422 {
     private static FastReader SC = new FastReader();
     private static int N, M;
-    private static boolean[][] C;
-    private static int CNT;
+    private static boolean[][] COMBINATION;
 
     public static void main(String[] args) {
         input();
@@ -14,33 +13,35 @@ public class BOJ2422 {
     }
 
     private static void solve() {
+        int cnt = 0;
         for (int i = 1; i <= N - 2; i++) {
             for (int j = i + 1; j <= N - 1; j++) {
-                if (!C[i][j]) {
-                    for (int k = j + 1; k <= N; k++) {
-                        if (!C[j][k] && !C[i][k]) {
-                            // System.out.println(i + " " + j + " " + k);
-                            CNT++;
-                        }
+                if (COMBINATION[i][j] || COMBINATION[j][i]) {
+                    continue;
+                }
+                for (int k = j + 1; k <= N; k++) {
+                    if (COMBINATION[i][k] || COMBINATION[k][i] || COMBINATION[j][k]
+                            || COMBINATION[k][j]) {
+                        continue;
                     }
+                    System.out.println(i + " " + j + " " + k);
+                    cnt++;
                 }
             }
         }
-        System.out.println(CNT);
+
+        System.out.println(cnt);
     }
-
-
 
     private static void input() {
         N = SC.nextInt();
         M = SC.nextInt();
-        C = new boolean[N + 1][N + 1];
+        COMBINATION = new boolean[N + 1][N + 1];
         for (int i = 0; i < M; i++) {
             int n1 = SC.nextInt();
             int n2 = SC.nextInt();
-            C[n1][n2] = true;
-            C[n2][n1] = true;
+            COMBINATION[n1][n2] = true;
+            COMBINATION[n2][n1] = true;
         }
-        CNT = 0;
     }
 }
