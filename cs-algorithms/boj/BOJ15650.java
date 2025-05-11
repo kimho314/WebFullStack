74e3919240b36cfbccc0f13cf45328f24bf897bf
Hoseop.kim <kimho314@gmail.com>
2025-05-12 02:28:03 +0900

    [feat]: modify BOJ15650

--- cs-algorithms/boj/BOJ15650.java
+++ cs-algorithms/boj/BOJ15650.java
@@ -1,36 +1,38 @@
 package boj;
 
 public class BOJ15650 {
-    static StringBuilder sb = new StringBuilder();
-    static FastReader sc = new FastReader();
-    static int N, M;
-    static int[] selected;
+    private static FastReader SC = new FastReader();
+    private static int N, M;
+    private static int[] SELECTED;
+    private static StringBuilder SB = new StringBuilder();
 
     public static void main(String[] args) {
-        N = sc.nextInt();
-        M = sc.nextInt();
-        selected = new int[M + 1];
-
-        recFunc(1);
-        System.out.println(sb);
+        input();
+        solve();
     }
 
-    static void recFunc(int k) {
-        if (k == M + 1) { // 다 골랐다!
-            // selected[1...M] 배열이 새롭게 탐색된 결과
-            for (int i = 1; i <= M; i++) {
-                sb.append(selected[i]).append(' ');
+    private static void input() {
+        N = SC.nextInt();
+        M = SC.nextInt();
+        SELECTED = new int[M];
+    }
+
+    private static void solve() {
+        recFunc(0, 0);
+        System.out.println(SB);
+    }
+
+    private static void recFunc(int prev, int k) {
+        if (k == M) {
+            for (int elem : SELECTED) {
+                SB.append(elem).append(" ");
             }
-            sb.append('\n');
-        }
-        else {
-            for (int cand = 1; cand <= N; cand++) {
-                if (cand <= selected[k - 1]) {
-                    continue;
-                }
-                selected[k] = cand;
-                recFunc(k + 1);
-                selected[k] = 0;
+            SB.append("\n");
+        } else {
+            for (int i = prev + 1; i <= N; i++) {
+                SELECTED[k] = i;
+                recFunc(i, k + 1);
+                SELECTED[k] = 0;
             }
         }
     }
