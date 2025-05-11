79ae02a5c7ae7671a3eb6ac9ce5cc2929f3ef7c1
Hoseop.kim <kimho314@gmail.com>
2025-05-12 02:37:46 +0900

    [feat]: modify BOJ15656

--- cs-algorithms/boj/BOJ15656.java
+++ cs-algorithms/boj/BOJ15656.java
@@ -1,36 +1,45 @@
 package boj;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class BOJ15656 {
-    static FastReader SC = new FastReader();
-    static int N, M;
-    static int[] A, SELECTED;
-    static StringBuilder SB = new StringBuilder();
+    private static FastReader SC = new FastReader();
+    private static int N, M;
+    private static int[] A;
+    private static int[] SELECTED;
+    private static StringBuilder SB = new StringBuilder();
 
     public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    private static void input() {
         N = SC.nextInt();
         M = SC.nextInt();
-        A = new int[N + 1];
-        SELECTED = new int[M + 1];
-        for (int i = 1; i <= N; i++) {
+        A = new int[N];
+        for (int i = 0; i < N; i++) {
             A[i] = SC.nextInt();
         }
+        SELECTED = new int[M];
+    }
+
+    private static void solve() {
         Arrays.sort(A);
-        recFunc(1);
+
+        recFunc(0);
 
         System.out.println(SB);
     }
 
     private static void recFunc(int k) {
-        if (k == M + 1) {
-            for (int i = 1; i <= M; i++) {
-                SB.append(SELECTED[i]).append(' ');
+        if (k == M) {
+            for (int elem : SELECTED) {
+                SB.append(elem).append(" ");
             }
-            SB.append('\n');
-        }
-        else {
-            for (int i = 1; i <= N; i++) {
+            SB.append("\n");
+        } else {
+            for (int i = 0; i < N; i++) {
                 SELECTED[k] = A[i];
                 recFunc(k + 1);
                 SELECTED[k] = 0;
