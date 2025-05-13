692039c4512ab167bc309eefbcd4aadb91680bb8
Hoseop.kim <kimho314@gmail.com>
2025-05-13 15:31:34 +0900

    [feat]: modify BOJ15654

--- cs-algorithms/boj/BOJ15654.java
+++ cs-algorithms/boj/BOJ15654.java
@@ -1,46 +1,49 @@
 package boj;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class BOJ15654 {
-    static FastReader SC = new FastReader();
-    static StringBuilder SB = new StringBuilder();
-    static int N, M;
-    static int[] SELECTED;
-    static int[] A;
+    private static FastReader SC = new FastReader();
+    private static int N, M;
+    private static boolean[] USED;
+    private static int[] SELECTED, NUMS;
+    private static StringBuilder SB = new StringBuilder();
 
     public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    private static void input() {
         N = SC.nextInt();
         M = SC.nextInt();
-        SELECTED = new int[M + 1];
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = SC.nextInt();
+        USED = new boolean[N];
+        SELECTED = new int[M];
+        NUMS = new int[N];
+        for (int i = 0; i < N; i++) {
+            NUMS[i] = SC.nextInt();
         }
-        Arrays.sort(A);
-        recFunc(1);
+    }
+
+    private static void solve() {
+        Arrays.sort(NUMS);
+        recFunc(0);
         System.out.println(SB);
     }
 
     private static void recFunc(int k) {
-        if (k == M + 1) {
-            for (int i = 1; i <= M; i++) {
-                SB.append(SELECTED[i]).append(' ');
+        if (k == M) {
+            for (int elem : SELECTED) {
+                SB.append(elem).append(" ");
             }
-            SB.append('\n');
-        }
-        else {
-            for (int cand = 1; cand <= N; cand++) {
-                boolean isUsed = false;
-                for (int i = 0; i < k; i++) {
-                    if (A[cand] == SELECTED[i]) {
-                        isUsed = true;
-                        break;
-                    }
-                }
-                if (!isUsed) {
-                    SELECTED[k] = A[cand];
+            SB.append("\n");
+        } else {
+            for (int i = 0; i < N; i++) {
+                if (!USED[i]) {
+                    USED[i] = true;
+                    SELECTED[k] = NUMS[i];
                     recFunc(k + 1);
+                    USED[i] = false;
                     SELECTED[k] = 0;
                 }
             }
