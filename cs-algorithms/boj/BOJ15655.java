85e74bf6c05a1e792e39122806c8e708a54fada8
Hoseop.kim <kimho314@gmail.com>
2025-05-13 20:03:36 +0900

    [feat]: modify BOJ15655

--- cs-algorithms/boj/BOJ15655.java
+++ cs-algorithms/boj/BOJ15655.java
@@ -1,41 +1,47 @@
 package boj;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class BOJ15655 {
-    static FastReader SC = new FastReader();
-    static int N, M;
-    static int[] S, SELECTED;
-    static StringBuilder SB = new StringBuilder();
+    private static FastReader SC = new FastReader();
+    private static int N, M;
+    private static int[] SELECTED, NUMS;
+    private static StringBuilder SB = new StringBuilder();
 
     public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    private static void input() {
         N = SC.nextInt();
         M = SC.nextInt();
-        S = new int[N + 1];
-        SELECTED = new int[M + 1];
-        for (int i = 1; i <= N; i++) {
-            S[i] = SC.nextInt();
+        SELECTED = new int[M];
+        NUMS = new int[N];
+        for (int i = 0; i < N; i++) {
+            NUMS[i] = SC.nextInt();
         }
+    }
 
-        Arrays.sort(S);
-        recFunc(1);
+    private static void solve() {
+        Arrays.sort(NUMS);
+
+        recFunc(-1, 0);
+
         System.out.println(SB);
     }
 
-    private static void recFunc(int k) {
-        if (k == M + 1) {
-            for (int i = 1; i <= M; i++) {
-                SB.append(SELECTED[i]).append(' ');
+    private static void recFunc(int prev, int k) {
+        if (k == M) {
+            for (int elem : SELECTED) {
+                SB.append(elem).append(" ");
             }
-            SB.append('\n');
-        }
-        else {
-            for (int i = 1; i <= N; i++) {
-                if (S[i] > SELECTED[k - 1]) {
-                    SELECTED[k] = S[i];
-                    recFunc(k + 1);
-                    SELECTED[k] = 0;
-                }
+            SB.append("\n");
+        } else {
+            for (int i = prev + 1; i < N; i++) {
+                SELECTED[k] = NUMS[i];
+                recFunc(i, k + 1);
+                SELECTED[k] = 0;
             }
         }
     }
