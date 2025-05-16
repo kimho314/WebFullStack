e972bd0fb13669745832f970b8b42cd38683de86
Hoseop.kim <kimho314@gmail.com>
2025-05-16 23:17:35 +0900

    [feat]: modify BOJ2805

--- cs-algorithms/boj/BOJ2805.java
+++ cs-algorithms/boj/BOJ2805.java
@@ -1,44 +1,50 @@
 package boj;
 
+import java.util.*;
+
 public class BOJ2805 {
-    static FastReader FB = new FastReader();
-    static int N;
-    static int M;
-    static int[] TREES;
+    private static FastReader SC = new FastReader();
+    private static int N, M;
+    private static int[] H;
 
     public static void main(String[] args) {
-        N = FB.nextInt();
-        M = FB.nextInt();
-        TREES = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            TREES[i] = FB.nextInt();
-        }
+        input();
+        solve();
+    }
 
-        long l = 1L;
-        long r = 2000000000L;
-        long height = 0L;
+    private static void input() {
+        N = SC.nextInt();
+        M = SC.nextInt();
+        H = new int[N];
+        for (int i = 0; i < N; i++) {
+            H[i] = SC.nextInt();
+        }
+    }
+
+    private static void solve() {
+        long res = 0;
+        long l = 0;
+        long r = 1_000_000_000;
         while (l <= r) {
             long mid = (l + r) / 2;
-            if (determination(mid)) {
+
+            if (check(mid)) {
+                res = mid;
                 l = mid + 1;
-                height = mid;
-            }
-            else {
+            } else {
                 r = mid - 1;
             }
         }
-
-        System.out.println(height);
+        System.out.println(res);
     }
 
-    private static boolean determination(long mid) {
-        long sum = 0L;
-        for (int i = 1; i <= N; i++) {
-            if (mid <= TREES[i]) {
-                sum += TREES[i] - mid;
+    private static boolean check(long h) {
+        long sum = 0;
+        for (int i = 0; i < N; i++) {
+            if (H[i] > h) {
+                sum += (H[i] - h);
             }
         }
-
         return sum >= M;
     }
 }
