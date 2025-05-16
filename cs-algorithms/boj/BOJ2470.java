eaf431d2d53aaecd423db9eaee2da55d415ededb
Hoseop.kim <kimho314@gmail.com>
2025-05-17 00:22:13 +0900

    [feat]: modify BOJ2470

--- cs-algorithms/boj/BOJ2470.java
+++ cs-algorithms/boj/BOJ2470.java
@@ -1,57 +1,49 @@
 package boj;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class BOJ2470 {
-    static int N;
-    static FastReader sc = new FastReader();
-    static int[] NUMS;
+    private static FastReader SC = new FastReader();
+    private static int N;
+    private static int[] W;
 
     public static void main(String[] args) {
-        N = sc.nextInt();
-        NUMS = new int[N];
-        for (int i = 0; i < N; i++) {
-            NUMS[i] = sc.nextInt();
-        }
-
-        Arrays.sort(NUMS);
-
-        int bestSum = Integer.MAX_VALUE;
-        int v1 = 0;
-        int v2 = 0;
-        for (int i = 0; i < N; i++) {
-            // -num 보다 오른쪽에 있는 숫자들 중 가장 왼쪽 숫자의 인덱스 반환
-            int res = binarySearch(NUMS, i + 1, N - 1, (-1 * NUMS[i]));
-
-            // 위에서 얻은 인덱스 or 인덱스 -1 중 num과의 합이 가장 0에 가까운 값 선택
-            if (i < res - 1 && Math.abs(NUMS[i] + NUMS[res - 1]) < bestSum) {
-                bestSum = Math.abs(NUMS[i] + NUMS[res - 1]);
-                v1 = NUMS[i];
-                v2 = NUMS[res - 1];
-            }
-
-            if (res < N && Math.abs(NUMS[i] + NUMS[res]) < bestSum) {
-                bestSum = Math.abs(NUMS[i] + NUMS[res]);
-                v1 = NUMS[i];
-                v2 = NUMS[res];
-            }
-        }
-        System.out.println(v1 + " " + v2);
+        input();
+        solve();
     }
 
-    static int binarySearch(int[] arr, int left, int right, int key) {
-        int res = right + 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] < key) {
-                left = mid + 1;
+    private static void input() {
+        N = SC.nextInt();
+        W = new int[N];
+        for (int i = 0; i < N; i++) {
+            W[i] = SC.nextInt();
+        }
+    }
+
+    private static void solve() {
+        Arrays.sort(W);
+
+        int res = 2_000_000_000;
+        int l = 0;
+        int r = N - 1;
+        String s = null;
+        while (l < r) {
+            int sum = W[l] + W[r];
+            // System.out.println(l + " " + r + " " + sum + " " + res);
+            if (res > Math.abs(sum)) {
+                res = Math.abs(sum);
+                s = W[l] + " " + W[r];
             }
-            else {
-                right = mid - 1;
-                res = mid;
+
+            if (sum == 0) {
+                break;
+            } else if (sum < 0) {
+                l++;
+            } else {
+                r--;
             }
         }
 
-        return res;
+        System.out.println(s);
     }
 }
