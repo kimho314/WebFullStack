b461bbdddef552c597884c61637915858debee62
Hoseop.kim <kimho314@gmail.com>
2025-05-26 17:20:34 +0300

    [feat]: modify BOJ14888

--- cs-algorithms/boj/BOJ14888.java
+++ cs-algorithms/boj/BOJ14888.java
@@ -1,60 +1,67 @@
 package boj;
 
+import java.util.*;
+
 public class BOJ14888 {
-    static int N;
-    static int[] NUMS;
-    static int[] OPERATORS = new int[4];
-    static int[] ORDERS;
-    static int MAX = Integer.MIN_VALUE;
-    static int MIN = Integer.MAX_VALUE;
-    static FastReader sc = new FastReader();
-    static StringBuilder sb = new StringBuilder();
+    private static FastReader SC = new FastReader();
+    private static int N;
+    private static int[] A;
+    private static int[] CNT;
+    private static int[] SELECTED;
+    private static int MIN, MAX;
 
     public static void main(String[] args) {
-        N = sc.nextInt();
-        NUMS = new int[N + 1];
-        ORDERS = new int[N];
-        for (int i = 1; i <= N; i++) {
-            NUMS[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < 4; i++) {
-            OPERATORS[i] = sc.nextInt();
-        }
-
-        recFunc(1);
-        sb.append(MAX).append("\n").append(MIN);
-        System.out.println(sb);
+        input();
+        solve();
     }
 
-    static void recFunc(int k) {
-        if (k == N) { // 계산 결과 값 구하기
-            int res = NUMS[1];
-            for (int i = 1; i < N; i++) {
-                if (ORDERS[i] == 1) {
-                    res = res + NUMS[i + 1];
-                }
-                if (ORDERS[i] == 2) {
-                    res = res - NUMS[i + 1];
-                }
-                if (ORDERS[i] == 3) {
-                    res = res * NUMS[i + 1];
-                }
-                if (ORDERS[i] == 4) {
-                    res = res / NUMS[i + 1];
+    private static void input() {
+        N = SC.nextInt();
+        A = new int[N];
+        CNT = new int[4];
+        for (int i = 0; i < N; i++) {
+            A[i] = SC.nextInt();
+        }
+        for (int i = 0; i < 4; i++) {
+            CNT[i] = SC.nextInt();
+        }
+        SELECTED = new int[N - 1];
+        MIN = Integer.MAX_VALUE;
+        MAX = Integer.MIN_VALUE;
+    }
+
+    private static void solve() {
+        recFunc(0);
+
+        System.out.println(MAX);
+        System.out.println(MIN);
+    }
+
+    private static void recFunc(int k) {
+        if (k == N - 1) {
+            int sum = A[0];
+            for (int i = 0; i < N - 1; i++) {
+                if (SELECTED[i] == 0) {
+                    sum += A[i + 1];
+                } else if (SELECTED[i] == 1) {
+                    sum -= A[i + 1];
+                } else if (SELECTED[i] == 2) {
+                    sum *= A[i + 1];
+                } else {
+                    sum /= A[i + 1];
                 }
             }
-            MIN = Math.min(res, MIN);
-            MAX = Math.max(res, MAX);
-        }
-        else {
+            // System.out.println(Arrays.toString(SELECTED));
+            MIN = Math.min(MIN, sum);
+            MAX = Math.max(MAX, sum);
+        } else {
             for (int i = 0; i < 4; i++) {
-                if (OPERATORS[i] != 0) {
-                    OPERATORS[i]--;
-                    ORDERS[k] = i + 1; // 1 = +, 2 = -, 3 = *, 4 = /
+                if (CNT[i] > 0) {
+                    CNT[i]--;
+                    SELECTED[k] = i;
                     recFunc(k + 1);
-                    OPERATORS[i]++;
-                    ORDERS[k] = 0;
+                    CNT[i]++;
+                    SELECTED[k] = -1;
                 }
             }
         }
