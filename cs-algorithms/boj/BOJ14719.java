4c12afa35210292b4747975d4c63b867c3dbb681
Hoseop.kim <kimho314@gmail.com>
2025-05-26 18:40:33 +0300

    [feat]: modify BOJ14719

--- cs-algorithms/boj/BOJ14719.java
+++ cs-algorithms/boj/BOJ14719.java
@@ -1,43 +1,49 @@
 package boj;
 
+import java.util.*;
+
 public class BOJ14719 {
     private static FastReader SC = new FastReader();
     private static int H, W;
-    private static int[] BLOCKS;
+    private static int[] WATER;
 
     public static void main(String[] args) {
         input();
         solve();
     }
 
+    private static void input() {
+        H = SC.nextInt();
+        W = SC.nextInt();
+        WATER = new int[W];
+        for (int i = 0; i < W; i++) {
+            WATER[i] = SC.nextInt();
+        }
+    }
+
     private static void solve() {
         int res = 0;
 
         for (int i = 1; i < W - 1; i++) {
-            int left = 0;
-            for (int j = 0; j < i; j++) {
-                left = Math.max(left, BLOCKS[j]);
-            }
-
-            int right = 0;
+            int right = i;
             for (int j = i + 1; j < W; j++) {
-                right = Math.max(right, BLOCKS[j]);
+                if (WATER[right] < WATER[j]) {
+                    right = j;
+                }
+            }
+            int left = i;
+            for (int j = 0; j < i; j++) {
+                if (WATER[left] < WATER[j]) {
+                    left = j;
+                }
             }
 
-            if (BLOCKS[i] < left && BLOCKS[i] < right) {
-                res += Math.min(left, right) - BLOCKS[i];
+            if (right != i && left != i) {
+                int sum = Math.min(WATER[left], WATER[right]) - WATER[i];
+                res += sum;
             }
         }
 
         System.out.println(res);
-    }
-
-    private static void input() {
-        H = SC.nextInt();
-        W = SC.nextInt();
-        BLOCKS = new int[W];
-        for (int i = 0; i < W; i++) {
-            BLOCKS[i] = SC.nextInt();
-        }
     }
 }
