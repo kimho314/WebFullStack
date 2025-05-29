20d63ecaf3f978141d9904c3909d8c88142b0360
Hoseop.kim <kimho314@gmail.com>
2025-05-29 17:26:11 +0300

    [feat]: modify BOJ2075

--- cs-algorithms/boj/BOJ2075.java
+++ cs-algorithms/boj/BOJ2075.java
@@ -1,36 +1,33 @@
 package boj;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class BOJ2075 {
-    static FastReader SC = new FastReader();
-    static int N;
-    static PriorityQueue<Integer> PQ = new PriorityQueue<>(Comparator.reverseOrder());
-    ;
+    private static FastReader SC = new FastReader();
+    private static int N;
+    private static PriorityQueue<Integer> PQ = new PriorityQueue<>(Comparator.reverseOrder());;
 
     public static void main(String[] args) {
         input();
         solve();
     }
 
+    private static void input() {
+        N = SC.nextInt();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                PQ.add(SC.nextInt());
+            }
+        }
+    }
+
     private static void solve() {
         int res = 0;
-        int cnt = 0;
-        while (cnt < N) {
-            cnt++;
+        while (N > 0) {
+            N--;
             res = PQ.poll();
         }
 
         System.out.println(res);
-    }
-
-    private static void input() {
-        N = SC.nextInt();
-
-        for (int i = 0; i < N * N; i++) {
-            int num = SC.nextInt();
-            PQ.add(num);
-        }
     }
 }
