6556914afdc5ff4fb4d0e7b10e03295a64c7e276
Hoseop.kim <kimho314@gmail.com>
2025-05-29 22:21:28 +0300

    [feat]: add BOJ12919

---
+++ cs-algorithms/boj/BOJ12919.java
@@ -0,0 +1,48 @@
+package boj;
+
+import java.util.*;
+
+public class BOJ12919 {
+    private static FastReader SC = new FastReader();
+    private static String S, T;
+    private static int RES = 0;
+
+    public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    private static void input() {
+        S = SC.next();
+        T = SC.next();
+    }
+
+    private static void solve() {
+        recFunc(T);
+
+        System.out.println(RES);
+    }
+
+    private static void recFunc(String s) {
+        if (s.length() < S.length()) {
+            return;
+        } else if (s.length() == S.length() && s.equals(S)) {
+            RES = 1;
+            return;
+        } else if (s.length() == S.length() && !s.equals(S)) {
+            return;
+        } else {
+            if (s.charAt(s.length() - 1) == 'A') {
+                String tmp = s.substring(0, s.length() - 1);
+                recFunc(tmp);
+            }
+            if (s.charAt(0) == 'B') {
+                StringBuilder sb = new StringBuilder();
+                sb.append(s).reverse();
+                sb.deleteCharAt(sb.length() - 1);
+                String tmp = sb.toString();
+                recFunc(tmp);
+            }
+        }
+    }
+}
