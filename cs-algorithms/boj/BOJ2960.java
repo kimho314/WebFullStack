b21ef48f4a11ec003fe97d2930a9557e435a0c7c
Hoseop.kim <kimho314@gmail.com>
2025-06-01 11:01:19 +0300

    [feat]: modify BOJ2960

--- cs-algorithms/boj/BOJ2960.java
+++ cs-algorithms/boj/BOJ2960.java
@@ -1,6 +1,6 @@
 package boj;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class BOJ2960 {
     private static FastReader SC = new FastReader();
@@ -11,45 +11,47 @@ public class BOJ2960 {
         solve();
     }
 
-    private static void solve() {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 2; i <= N; i++) {
-            list.add(i);
-        }
+    private static void input() {
+        N = SC.nextInt();
+        K = SC.nextInt();
+    }
 
+    private static void solve() {
+        boolean[] nums = new boolean[1001];
         int cnt = 0;
         int res = 0;
-
-        while (!list.isEmpty()) {
-            int n = list.remove(0);
-            cnt++;
-            if (cnt == K) {
-                res = n;
-                break;
-            }
-
-            boolean isK = false;
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i) % n == 0) {
+        for (int i = 2; i <= N; i++) {
+            if (!nums[i] && isPrime(i)) {
+                nums[i] = true;
+                cnt++;
+                // System.out.println(i + " " + cnt);
+                if (cnt == K) {
+                    res = i;
+                }
+                for (int j = 2; j * i <= N; j++) {
+                    if (nums[i * j]) {
+                        continue;
+                    }
+                    nums[i * j] = true;
                     cnt++;
-                    int x = list.remove(i);
+                    // System.out.println(i * j + " " + cnt);
                     if (cnt == K) {
-                        res = x;
-                        isK = true;
-                        break;
+                        res = i * j;
                     }
                 }
-            }
-            if (isK) {
-                break;
             }
         }
 
         System.out.println(res);
     }
 
-    private static void input() {
-        N = SC.nextInt();
-        K = SC.nextInt();
+    private static boolean isPrime(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
