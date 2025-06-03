4622b23e975a833e8f5b3c60f7ca4e812fa78cc3
Hoseop.kim <kimho314@gmail.com>
2025-06-03 22:24:04 +0300

    [feat]: modify BOJ10972

--- cs-algorithms/boj/BOJ10972.java
+++ cs-algorithms/boj/BOJ10972.java
@@ -10,35 +10,40 @@ public class BOJ10972 {
         solve();
     }
 
-    private static void solve() {
-        if (hasNextPerm()) {
-            StringBuilder sb = new StringBuilder();
-            for (int i : NUMS) {
-                sb.append(i).append(" ");
-            }
-            sb.append("\n");
-            System.out.println(sb);
-        } else {
-            System.out.println(-1);
+    private static void input() {
+        N = SC.nextInt();
+        NUMS = new int[N];
+        for (int i = 0; i < N; i++) {
+            NUMS[i] = SC.nextInt();
         }
     }
 
-    private static boolean hasNextPerm() {
+    private static void solve() {
+        StringBuilder sb = new StringBuilder();
+        if (nextPerm()) {
+            for (int elem : NUMS) {
+                sb.append(elem).append(" ");
+            }
+        } else {
+            sb.append(-1);
+        }
 
+        System.out.println(sb);
+    }
+
+    private static boolean nextPerm() {
         int i = N - 1;
         while (i > 0 && NUMS[i - 1] > NUMS[i]) {
             i--;
         }
-
         if (i == 0) {
             return false;
         }
 
         int j = N - 1;
-        while (NUMS[i - 1] > NUMS[j]) {
+        while (NUMS[i - 1] >= NUMS[j]) {
             j--;
         }
-
         swap(i - 1, j);
 
         j = N - 1;
@@ -47,20 +52,13 @@ public class BOJ10972 {
             i++;
             j--;
         }
+
         return true;
     }
 
-    private static void swap(int i, int j) {
-        int tmp = NUMS[i];
-        NUMS[i] = NUMS[j];
-        NUMS[j] = tmp;
-    }
-
-    private static void input() {
-        N = SC.nextInt();
-        NUMS = new int[N];
-        for (int i = 0; i < N; i++) {
-            NUMS[i] = SC.nextInt();
-        }
+    private static void swap(int idx1, int idx2) {
+        int tmp = NUMS[idx1];
+        NUMS[idx1] = NUMS[idx2];
+        NUMS[idx2] = tmp;
     }
 }
