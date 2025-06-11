36b8a84f824df4f595ff0a39468bb69dcc1cd8dc
Hoseop.kim <kimho314@gmail.com>
2025-06-11 18:40:26 +0300

    [feat]: add BOJ10973

--- cs-algorithms/boj/BOJ10973.java
+++ cs-algorithms/boj/BOJ10973.java
@@ -10,55 +10,57 @@ public class BOJ10973 {
         solve();
     }
 
-    private static void solve() {
-        boolean isPrev = getPrevPerm();
-        if (!isPrev) {
-            System.out.println(-1);
-        } else {
-            for (int num : NUMS) {
-                System.out.print(num + " ");
-            }
-        }
-    }
-
-    private static boolean getPrevPerm() {
-        int i = N - 1;
-        while (i > 0 && NUMS[i - 1] <= NUMS[i]) {
-            i--;
-        }
-
-        if (i <= 0) {
-            return false;
-        }
-
-        int j = i;
-        while (j < N - 1 && NUMS[i - 1] > NUMS[j + 1]) {
-            j++;
-        }
-
-        swap(i - 1, j);
-
-        j = N - 1;
-        while (i < j) {
-            swap(i, j);
-            i += 1;
-            j -= 1;
-        }
-
-        return true;
-    }
-
-    private static void swap(int i, int j) {
-        int temp = NUMS[i];
-        NUMS[i] = NUMS[j];
-        NUMS[j] = temp;
-    }
-
     private static void input() {
         N = SC.nextInt();
         NUMS = new int[N];
         for (int i = 0; i < N; i++) {
             NUMS[i] = SC.nextInt();
         }
+    }
+
+    private static void solve() {
+        if (prevPerm()) {
+            for (int i = 0; i < N; i++) {
+                System.out.print(NUMS[i] + " ");
+            }
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    private static boolean prevPerm() {
+        int idx = N - 1;
+        while (idx > 0) {
+            if (NUMS[idx] > NUMS[idx - 1]) {
+                idx--;
+            } else {
+                break;
+            }
+        }
+        if (idx == 0) {
+            return false;
+        }
+        int i = idx - 1;
+        int j = N - 1;
+        while (j > idx && NUMS[j] >= NUMS[i]) {
+            j--;
+        }
+        swap(i, j);
+
+        int l = idx;
+        int r = N - 1;
+        while (l < r) {
+            swap(r, l);
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    private static void swap(int i, int j) {
+        int tmp = NUMS[i];
+        NUMS[i] = NUMS[j];
+        NUMS[j] = tmp;
     }
 }
