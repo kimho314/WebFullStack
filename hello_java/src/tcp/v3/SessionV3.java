502186ee7e80d6396543983644eb71d488cf8757
Hoseop.kim <kimho314@gmail.com>
2025-08-03 21:27:26 +0900

    [feat]: implement enhanced TCP communication models with versioned Client, Server, and Session classes

---
+++ hello_java/src/tcp/v3/SessionV3.java
@@ -0,0 +1,45 @@
+package tcp.v3;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static util.MyLogger.log;
+
+public class SessionV3 implements Runnable {
+    private final Socket socket;
+
+    public SessionV3(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                String received = input.readUTF();
+                log("client -> server: " + received);
+
+                if (received.equals("exit")) {
+                    break;
+                }
+
+                String toSend = received + " World!";
+                output.writeUTF(toSend);
+                log("client <- server: " + toSend);
+
+                // 자원 정리
+                log("연결 종료: " + socket);
+                input.close();
+                output.close();
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
