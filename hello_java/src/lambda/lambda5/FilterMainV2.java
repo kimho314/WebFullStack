ad19e2027b2b6f868816d26b51bd9a0047b26247
Hoseop.kim <kimho314@gmail.com>
2025-08-06 17:40:59 +0900

    [feat]: add lambda examples showcasing filtering with predicates in `lambda5` package

---
+++ hello_java/src/lambda/lambda5/FilterMainV2.java
@@ -0,0 +1,27 @@
+package lambda.lambda5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FilterMainV2 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> evenNumbers = filter(numbers, (value) -> value % 2 == 0);
+        System.out.println("evenNumbers = " + evenNumbers);
+
+        List<Integer> oddNumbers = filter(numbers, (value) -> value % 2 == 1);
+        System.out.println("oddNumbers = " + oddNumbers);
+    }
+
+    private static List<Integer> filter(List<Integer> list, Predicate<Integer> predicate) {
+        List<Integer> filtered = new ArrayList<>();
+        for (Integer number : list) {
+            if (predicate.test(number)) {
+                filtered.add(number);
+            }
+        }
+        return filtered;
+    }
+}
