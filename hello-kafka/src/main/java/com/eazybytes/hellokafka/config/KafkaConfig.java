9bb9bc738d5af1d8592e44985bf004283bc0f330
Hoseop.kim <kimho314@gmail.com>
2025-09-12 15:14:19 +0900

    [feat]: init hello kafka project

---
+++ hello-kafka/src/main/java/com/eazybytes/hellokafka/config/KafkaConfig.java
@@ -0,0 +1,9 @@
+package com.eazybytes.hellokafka.config;
+
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@EnableKafka
+@EnableKafkaStreams
+public class KafkaConfig {
+}
---
+++ hello-kafka/src/test/java/com/eazybytes/hellokafka/HelloKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.hellokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
