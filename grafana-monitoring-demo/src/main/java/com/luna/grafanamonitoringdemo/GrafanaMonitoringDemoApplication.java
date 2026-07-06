2abf99d743a9767f4b4c72ef40c7e2f0b502715a
Hoseop.kim <kimho314@gmail.com>
2026-07-06 17:59:03 +0300

    [feat]: add grafana monitoring system project

---
+++ grafana-monitoring-demo/src/main/java/com/luna/grafanamonitoringdemo/GrafanaMonitoringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luna.grafanamonitoringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaMonitoringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrafanaMonitoringDemoApplication.class, args);
+    }
+
+}
---
+++ grafana-monitoring-demo/src/test/java/com/luna/grafanamonitoringdemo/GrafanaMonitoringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luna.grafanamonitoringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaMonitoringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
